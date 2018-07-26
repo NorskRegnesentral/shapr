43081109e22c753b7ed1e921d5917d078872231a
Martin Jullum <jullum@nr.no>
2018-07-26 14:19:35 +0200

    Testing vs master script

---
+++ scripts/testing_vs_master.R
@@ -0,0 +1,99 @@
+
+#### PAPER EXPERIMENT FRAMEWORK ####
+#### Use the current setup for all experiements in the paper to ease reproducablity etc.
+
+#### Example 1 ####
+# Linear model with independent Gaussian features
+
+rm(list = ls())
+
+library(shapr)
+library(data.table)
+library(mvtnorm)
+library(condMVNorm)
+
+source("paper_scripts/paper_helper_funcs.R")
+
+mu.list = list(c(0,0,0))
+Sigma.list <- list(matrix(c(1,0,0,
+                            0,1,0,
+                            0,0,1),ncol=3))
+pi.G <- 1
+
+sd = 0.1
+
+nTrain <- 10000
+nTest <- 10
+
+
+#### Defining the true distribution of the variables and the model------
+
+samp_variables <- function(n,pi.G,mu.list,Sigma.list){
+
+    X <- joint.samp.func(n = n,
+                         pi.G,
+                         mu.list,
+                         Sigma.list)
+    return(X)
+}
+
+samp_model <- function(n,X,sd){
+    y <- X[,1] + X[,2] + X[,3] + rnorm(n = n,mean=0,sd=sd)
+}
+
+
+
+
+#### Sampling train and test data ---------
+
+set.seed(123)
+XYtrain <- data.table(samp_variables(n = nTrain,
+                                     pi.G = pi.G,
+                                     mu.list = mu.list,
+                                     Sigma.list = Sigma.list))
+XYtrain[,y:=samp_model(.N,.SD,sd=sd)]
+Xtrain <- copy(XYtrain)
+Xtrain[,y:=NULL]
+
+XYtest <- data.table(samp_variables(n = nTest,
+                                    pi.G = pi.G,
+                                    mu.list = mu.list,
+                                    Sigma.list = Sigma.list))
+XYtest[,y:=samp_model(.N,.SD,sd=sd)]
+Xtest <- copy(XYtest)
+Xtest[,y:=NULL]
+
+#### Fitting the model ----------
+
+model = lm(y~.,data=XYtrain)
+
+pred_zero = XYtrain[, mean(y)]
+m = ncol(Xtrain)
+exact = TRUE
+nrow=1e4
+
+#### Pre computation before kernel shap --------------------------------
+l <- prepare_kernelShap(
+    m = m,
+    Xtrain = Xtrain,
+    Xtest = Xtest,
+    exact = TRUE,
+    nrows = 1e4,
+    scale = F,
+    distance_metric = "Euclidean")
+
+#### Computing the various Shapley approximations --------
+
+w_threshold = 1 # For a fairer comparison, all models use the same number of samples (n_threshold)
+n_threshold = 10^3
+
+Shapley.approx.test = compute_kernelShap(model = model,
+                                         l,
+                                         sigma = 0.1,
+                                         w_threshold = w_threshold,
+                                         n_threshold = n_threshold,
+                                         verbose = FALSE,
+                                         gaussian_sample = FALSE,
+                                         pred_zero=pred_zero,
+                                         kernel_metric = "Gaussian_old")
+
