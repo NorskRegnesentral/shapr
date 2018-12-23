ef84cf49d22dd18ee45cba480498d3db7ad79baa
Martin Jullum <jullum@nr.no>
2018-12-23 22:33:26 +0100

    Parallelization of 10-dim simulations

--- paper_experiments/executing_experiment_A_dim_10.R
+++ paper_experiments/executing_experiment_A_dim_10.R
@@ -1,15 +1,21 @@
 
-seed.vec <- 1:10 + 1234 # We fix this seed
+library(doParallel)
+cl <- makeCluster(5)
+registerDoParallel(cl)
+seed.vec <- 1:5 + 1234 # We fix this seed
 
-rho.vec <- seq(0,0.98,length.out=50)[c(seq(1,50,by=2),seq(2,50,by=2))]
+rho.vec <- seq(0,0.95,length.out=20)[c(seq(1,20,by=2),seq(2,20,by=2))]
 
-for (this.seed in seed.vec){
-    for (rho in rho.vec){
+foreach (rho=rho.vec) %dopar%{
+    for (this.seed in seed.vec){
         source("paper_experiments/experiment_A_dim_10_Linear_Linear_Gaussian.R")
 
         print(paste0("Just finished computation for rho = ",rho," with seed ",this.seed))
     }
+
 }
+
+stopCluster(cl)
 
 
 
