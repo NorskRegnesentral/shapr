81da6ac26647db818527b0e81434a189cdebada6
Jens Christian Wahl <jens.c.wahl@gmail.com>
2019-07-11 11:11:31 +0200

    Tests for R/transformation.R (#77)

--- R/transformation.R
+++ R/transformation.R
@@ -3,20 +3,21 @@
 #' Handled in this way in order to allow using the apply function over this function
 #' @param zx Vector where the first part is the Gaussian data, and last part is
 #' the data with the original transformation
-#' @param n_y How many elements of \code{yx} that belongs to the y-part (new data)
-#' @param type The quantile type used when back-transforming. 7 (default) is the default in stats::quantile().
+#' @param n_z How many elements of \code{zx} that belongs to the z-part (new data)
+#' @param type The quantile type used when back-transforming. 7 (default) is the default in \code{stats::quantile()}.
 #'
 #' @return Vector of transformed new data
 #' @export
 #'
 #' @author Martin Jullum
 inv_gaussian_transform <- function(zx, n_z, type = 7) {
+  if (n_z >= length(zx)) stop("n_z should be less than length(zx)")
   ind <- 1:n_z
   z <- zx[ind]
   x <- zx[-ind]
   u <- stats::pnorm(z)
-  xNew <- stats::quantile(x, u, type = type)
-  return(xNew)
+  x_new <- stats::quantile(x, probs = u, type = type)
+  return(x_new)
 }
 
 
@@ -25,21 +26,21 @@ inv_gaussian_transform <- function(zx, n_z, type = 7) {
 #' Handled in this way in order to allow using the apply function over this function
 #' @param yx Vector where the first part is the new data to transform,
 #' and last part is the data with the original transformation
-#' @param n_z How many elements of \code{zx} that belongs to the z-part (Gaussian data)
+#' @param n_y How many elements of \code{yx} that belongs to the y-part (Gaussian data)
 #'
 #' @return Vector of back-transformed Gaussian data
 #' @export
 #'
 #' @author Martin Jullum
 gaussian_transform_separate <- function(yx, n_y) {
+  if (n_y >= length(yx)) stop("n_y should be less than length(yx)")
   ind <- 1:n_y
-  y <- yx[ind]
   x <- yx[-ind]
-  tmp <- rank(c(y, x))[1:length(y)]
+  tmp <- rank(yx)[ind]
   tmp <- tmp - rank(tmp) + 0.5
-  u.y <- tmp / (length(x) + 1)
-  z.y <- stats::qnorm(u.y)
-  return(z.y)
+  u_y <- tmp / (length(x) + 1)
+  z_y <- stats::qnorm(u_y)
+  return(z_y)
 }
 
 #' Transforms a sample to standardized normal (dimension 1)
