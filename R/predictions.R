4fcb665505d94a9b9a1cfa9ac4e65688bd103fbb
nikolase90 <n_sellereite@hotmail.com>
2019-08-26 15:30:42 +0200

    Fixes bug

--- R/predictions.R
+++ R/predictions.R
@@ -110,7 +110,7 @@ predictions <- function(model,
   setkey(DTp, wcomb)
 
 
-  DTp[!(wcomb %in% c(1, 2^p)), p_hat := predict_model(model, SD), .SDcols = nms]
+  DTp[!(wcomb %in% c(1, 2^p)), p_hat := predict_model(model, .SD), .SDcols = nms]
   DTp[wcomb == 2^p, p_hat := predict_model(model, as.data.frame(Xtest))]
   DTp[wcomb == 1, p_hat := pred_zero]
 
