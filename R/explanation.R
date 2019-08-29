e19784f44954b4053964639b4a7afd3d21b1dec5
Camilla Lingjaerde <camilla_95@hotmail.com>
2019-08-29 10:26:19 +0200

    Auto stash before merge of "nikolai/shapley_class" and "origin/nikolai/shapley_class"

--- R/explanation.R
+++ R/explanation.R
@@ -9,22 +9,15 @@
 #' @export
 explain <- function(x, explainer, approach, prediction_zero, n_samples, ...) {
 
-  if (approach == "empirical") {
-    class(x) <- "empirical"
-  } else if (approach == "gaussian") {
-    class(x) <- "gaussian"
-  } else if (approach == "copula") {
-    class(x) <- "copula"
-  } else if (approach == "combined") {
-    class(x) <- "combined"
-  } else {
-    str_error <- paste(
-      "It seems that you passed a non-valid value for approach.",
-      "It should be either 'empirical', 'gaussian', 'copula' or",
-      "'combined'."
-    )
+  if(! approach %in% c('empirical','gaussian','copula','combined')){
+      str_error <- paste(
+        "It seems that you passed a non-valid value for approach.",
+        "It should be either 'empirical', 'gaussian', 'copula' or",
+        "'combined'."
+      )
     stop(str_error)
   }
+  class(x) <- approach
 
   UseMethod("explain", x)
 }
