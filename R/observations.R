91764fcc54f81409d3155e7df57e92a924fc954f
Camilla Lingjaerde <camilla_95@hotmail.com>
2019-08-29 07:44:50 +0200

    Adds tests for R/observations.R (part of #11)
    Plus adds extra checks to observation_impute

--- R/observations.R
+++ R/observations.R
@@ -1,5 +1,12 @@
 #' Get predictions
 #'
+#'
+#' @param W_kernel Array. Contains all nonscaled weights between training and testing observations for all combinations. Dimension \code{ntrain x nfeatures}.
+#' @param S Integer matrix of dimension \code{ncomb x nfeatures}, where \code{ncomb} equals the total number of sampled/non-sampled feature combinations and \code{nfeatures} equals the total number of unique features. Note that \code{nfeatures = ncol(xtrain)}.
+#' @param Xtrain Matrix, data.frame or data.table with the features from the training data. Dimension \code{ntrain x nfeatures}.
+#' @param Xtest Matrix, data.frame or data.table with the features of the observation whose predictions ought to be explained (test data). Dimension \code{1 x nfeatures} or \code{nfeatures x 1}.
+#' @param w_threshold Numeric. The number of samples to use is chosen to be the number of the largest weights needed to account for this fraction of the total weight.
+#'
 #' @inheritParams global_arguments
 #'
 #' @return List
@@ -8,9 +15,10 @@
 #'
 #' @author Nikolai Sellereite
 observation_impute <- function(W_kernel, S, Xtrain, Xtest, w_threshold = .7, noSamp_MC = 1e3) {
-
+  stopifnot(!is.null(dim(W_kernel)))
+  stopifnot(!is.null(dim(S)))
   stopifnot(all(dim(W_kernel) == c(nrow(Xtrain),nrow(S))))
-  stopifnot(all(is.integer(S)))
+  stopifnot(all(S%in%c(0,1)))
 
   ## Find weights for all combinations and training data
   DT <- as.data.table(W_kernel)
--- tests/testthat/test-observations.R
+++ tests/testthat/test-observations.R
@@ -1,33 +1,36 @@
 library(shapr)
-library(testthat)
-library(MASS)
-library(mvtnorm)
-library(data.table)
 
 context("test-observations.R")
 
 test_that("Test observation_impute", {
   # Example 1 -----------------
-  n=60
-  m=100
-  Sigma = cov(matrix(mvrnorm(m*n,0,1),nrow=n))
-  Xtrain = mvrnorm(n,mu=rep(0,m),Sigma=Sigma)
-  Xtest=mvrnorm(1,mu=rep(0,m),S)
-  S= t(combn())
-  res1.1 = shapr:::observation_impute(W_kernel, S, Xtrain, Xtest, w_threshold = .7, noSamp_MC = 1e3)
-  res1.2 = observation_impute(W_kernel, S, Xtrain, Xtest, w_threshold = .7)
-
+  # Test the default argument noSamp_MC
+  n <- 20
+  m <- 2
+  Sigma <- cov(matrix(MASS::mvrnorm(m * n, 0, 1), nrow = n))
+  Xtrain <- as.matrix(MASS::mvrnorm(n, mu = rep(0, m), Sigma = Sigma), ncol = m)
+  Xtest <- t(as.matrix(MASS::mvrnorm(1, mu = rep(0, m), Sigma)))
+  colnames(Xtrain) <- colnames(Xtest) <- c("X1", "X2")
+  Ytrain <- rnorm(n, 0, 2)
+  S <- matrix(c(1, 0, 0, 1), nrow = 2)
+  W_kernel <- matrix(rnorm(n * ncol(S), mean = 1 / n, sd = 1 / n^2), nrow = n)
+  res1.1 <- observation_impute(W_kernel, S, Xtrain, Xtest, w_threshold = .7, noSamp_MC = 1e3)
+  res1.2 <- observation_impute(W_kernel, S, Xtrain, Xtest, w_threshold = .7)
 
   # Example 2 ------------------
-
-  res2.1 = observation_impute(W_kernel, S, Xtrain, Xtest, w_threshold = .7, noSamp_MC = 1e3)
-  res2.2 = observation_impute(W_kernel, S, Xtrain, Xtest,noSamp_MC = 1e3)
+  # Test the default argument w_threshold
+  res2.1 <- observation_impute(W_kernel, S, Xtrain, Xtest, w_threshold = .7, noSamp_MC = 1e3)
+  res2.2 <- observation_impute(W_kernel, S, Xtrain, Xtest, noSamp_MC = 1e3)
 
   # Example 3 ------------------
+  # Test that w_threshold can be adjusted by alternative arguments, changing the dimension of the results.
+  res3.1 <- observation_impute(W_kernel, S, Xtrain, Xtest, w_threshold = .7)
+  res3.2 <- observation_impute(W_kernel, S, Xtrain, Xtest, w_threshold = 0.5)
 
   # Tests ----------------------
   expect_equal(res1.1, res1.2)
   expect_equal(res2.1, res2.2)
-  expect_error(observation_impute(W_kernel=1, S, Xtrain, Xtest))
-
+  expect_true(all(sapply(colnames(Xtrain), function(s) s %in% colnames(res2.1))))
+  expect_false(all(dim(res3.1) == dim(res3.2)))
+  expect_error(observation_impute(W_kernel = 1, S, Xtrain, Xtest))
 })
