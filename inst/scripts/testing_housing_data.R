f1efe46a938f55e515c273c12ca573bcd6fba6fc
Annabelle Redelmeier <aredelmeier@gmail.com>
2019-10-28 15:54:00 +0100

    slight changes to testing housing script

--- inst/scripts/testing_housing_data.R
+++ inst/scripts/testing_housing_data.R
@@ -1,11 +1,7 @@
-# library(dplyr)
-# library(xtable)
 library(xgboost)
 library(shapr)
 library(data.table)
 library(party)
-library(CatEncoders) ## for one hot encoding
-library(ggplot2)
 
 if(.Platform$OS.type=="windows"){
   projDir <- "M:"
@@ -61,7 +57,7 @@ x_train <- train_noNA[, x_var]
 x_test <- test_noNA[, x_var]
 
 y_train <- train_noNA[, y_var]
-y_train <- y_train / 1000000
+y_train <- y_train / 1000000 ## convert to millions
 
 
 ### SPECIAL STUFF  STARTS ###
@@ -80,7 +76,7 @@ model <- xgboost(
   verbose = FALSE
 )
 
-### SPEXIAL STUFF STARTS ###
+### SPECIAL STUFF STARTS ###
 model$dummyfunc <- dummyfunc
 ### SPECIAL STUFF ENDS ###
 
@@ -88,12 +84,17 @@ explainer <- shapr(x_train, model)
 
 p <- mean(y_train)
 
-explanation <- explain(
+start_time <- Sys.time()
+explanation <- explain( ## this takes a very long time to run = 21.55 minutes?
   x = as.data.table(x_test),
   approach = 'ctree',
   explainer = explainer,
   prediction_zero = p,
   sample = FALSE)
+
+end_time <- Sys.time()
+end_time - start_time
+
 
 ##        none   MSSubClass      MSZoning      LotArea        Street
 # 1: 0.1809212  0.002035604 -0.0181289541  0.050419428 -4.421495e-04
@@ -108,23 +109,5 @@ explanation <- explain(
 # 1446: 0.1809212 -0.032160189  0.0045814492 -0.001777592  6.406629e-04
 # 1447: 0.1809212  0.045843928  0.0011496367 -0.007289328  5.202563e-04
 
-plot(explanation)
-
-
-
-## to plot, do two things: (this is done in arrange_shapley)
-## 1. sum up the shapley values for the categorical variables
-## 2. add a column in x_test refering to the non-one-hot-encoded variable
-
-
-explanation2 <- arrange_shapley(original_test = test_data_input[1:6, ], explanation = explanation)
-
-plotDir <- paste(projDir, "BigInsight/Projects/Fraud/Subprojects/NAV/Annabelle/results/figures", sep = "/")
-
-setwd(plotDir)
-p2 <- plot(explanation2) # top_k_features = 4
-
-ggplot2::ggsave("shapley_value_prediction_housing_data_Gaussian_4features_2categorical_6testobs.png", plot = p2, device = NULL, path = NULL,
-                scale = 1, width = 6, height = 4,
-                dpi = 300, limitsize = TRUE)
+plot(explanation) ## this doesn't work?
 
