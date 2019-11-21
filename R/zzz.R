355df876050d99787c7ea4af4ebefda785972a44
Nikolai Sellereite <n_sellereite@hotmail.com>
2019-11-21 18:03:15 +0100

    Updates url and R-version in DESCRIPTION (#127)

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,10 @@
 .onLoad <- function(libname = find.package("shapr"), pkgname = "shapr") {
 
   # CRAN Note avoidance
-  if (getRversion() >= "2.15.1") {
-    utils::globalVariables(
-      c(
-        ".", ".N", ".I", ".GRP", ".SD"
-      )
+  utils::globalVariables(
+    c(
+      ".", ".N", ".I", ".GRP", ".SD"
     )
-  }
+  )
   invisible()
 }
