a5e58f2945be8208451bb95b49193a3dbf1c1341
Martin Jullum <jullum@nr.no>
2019-12-09 11:47:18 +0100

    Tests for feature labels and related warnings/messages (#165)

--- tests/testthat/test-a-shapley.R
+++ tests/testthat/test-a-shapley.R
@@ -5,7 +5,7 @@ context("test-shapley.R")
 
 if (as.numeric(version$minor) >= 6.0) RNGkind(sample.kind = "Rounding")
 
-test_that("Test functions in shapley.R", {
+test_that("Basic test functions in shapley.R", {
 
   # Load data -----------
   data("Boston", package = "MASS")
@@ -19,4 +19,88 @@ test_that("Test functions in shapley.R", {
   explainer <- shapr(x_train, model)
 
   expect_known_value(explainer, file = "test_objects/shapley_explainer_obj.rds")
+})
+
+
+test_that("Testing data input to shapr in shapley.R", {
+
+  data("Boston", package = "MASS")
+
+  x_var <- c("lstat", "rm", "dis", "indus")
+  y_var <- "medv"
+  x_var_sub <- x_var[1:2]
+  not_x_var <- "crim"
+  not_even_var <- "not_a_column_name"
+
+  x_train <- as.matrix(tail(Boston[, x_var], -6))
+  y_train <- tail(Boston[, y_var], -6)
+
+  xy_train_full_df <- tail(Boston[, ], -6)
+  xy_train_missing_lstat_df <- xy_train_full_df[,!(colnames(xy_train_full_df) == "lstat")]
+  xy_train_full_df_no_colnames <- xy_train_full_df
+  colnames(xy_train_full_df_no_colnames) <- NULL
+
+
+  x_test <- as.matrix(head(Boston[, x_var], 6))
+  x_test_full <- as.matrix(head(Boston[, ], 6))
+  x_test_reordered <- as.matrix(head(Boston[, rev(x_var)], 6))
+  xy_test_full_df <- head(Boston[, ], 6)
+  xy_test_missing_lstat_df <- xy_test_full_df[,!(colnames(xy_test_full_df) == "lstat")]
+  xy_test_full_df_no_colnames <- xy_test_full_df
+  colnames(xy_test_full_df_no_colnames) <- NULL
+
+
+
+  # Fitting models
+  model1 <- xgboost::xgboost(
+    data = x_train,
+    label = y_train,
+    nround = 20,
+    verbose = FALSE
+  )
+
+  formula <- as.formula(paste0("medv ~ ",paste0(x_var,collapse="+")))
+  model2 <- lm(formula = formula,
+               data = xy_train_full_df)
+  model3 <- ranger::ranger(formula = formula,
+                           data = xy_train_full_df,
+                           num.trees = 50)
+
+  # Just making up a fictive model class
+  model4 <- "cumstom_testmodel"
+  class(model4) = "testclass" # Class objects defined in helper-testclass.R
+
+
+  #### Running tests ####
+
+  # Expect silent
+  expect_silent(shapr(x_train, model1))
+  expect_silent(shapr(xy_train_full_df, model2))
+  expect_silent(shapr(xy_train_full_df, model3))
+  expect_silent(shapr(xy_train_full_df, model4,feature_labels = x_var))
+
+  # Expect message that feature_labels is ignored
+  expect_message(shapr(xy_train_full_df, model1, feature_labels = x_var_sub))
+  expect_message(shapr(xy_train_full_df, model1, feature_labels = x_var))
+
+  expect_message(shapr(xy_train_full_df, model2, feature_labels = x_var_sub))
+  expect_message(shapr(xy_train_full_df, model3, feature_labels = x_var_sub))
+
+  # Expect error, giving error message that throws indicates that the x misses columns used by the model
+  expect_error(shapr(xy_train_missing_lstat_df, model1))
+  expect_error(shapr(xy_train_missing_lstat_df, model2))
+
+  # Expect error that feature_labels is not in training data or used by the model
+  expect_error(shapr(xy_train_full_df, model4,feature_labels = not_x_var))
+  expect_error(shapr(xy_train_full_df, model4,feature_labels = not_even_var))
+
+  # Expect error, that feature_labels is missing
+  expect_error(shapr(xy_train_full_df, model4))
+
+  # Expect error when x_train don't have column names
+  expect_error(shapr(xy_train_full_df_no_colnames, model1, feature_labels = x_var_sub))
+  expect_error(shapr(xy_train_full_df_no_colnames, model2, feature_labels = x_var_sub))
+  expect_error(shapr(xy_train_full_df_no_colnames, model3, feature_labels = x_var_sub))
+  expect_error(shapr(xy_train_full_df_no_colnames, model4, feature_labels = x_var_sub))
+
 })
