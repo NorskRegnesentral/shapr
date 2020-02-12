33d6a324c3711f6d826e74d5c00ca3c7b0b74793
Martin Jullum <jullum@nr.no>
2020-02-12 11:35:16 +0100

    Prediction done only once. Speedup for high dim cases

--- inst/devel_scripts/true_shapley/new_functions.R
+++ inst/devel_scripts/true_shapley/new_functions.R
@@ -17,36 +17,34 @@
 
 
 
-cond_expec_new <- function(cond_list,explainer,x_test, prediction_zero){
+  cond_expec_new <- function(cond_list,explainer,x_test, prediction_zero,joint_prob_dt){
+
+# This approach does not work...
+#  cond_dt <- rbindlist(cond_list[-1],idcol = "cond_id",use.names = T)
+#  cond_dt[,cond_id := cond_id +1] # To refer to row in S below
+#  cond_dt <- merge(cond_dt,joint_prob_dt[,.(feat_comb_id,predict)],by="feat_comb_id")
+#  cond_dt[, expected_value := predict * cond_prob]
+#  cond_expec_dt_new <- cond_dt[,sum(expected_value),by=.(feat_comb_id,cond_id)]
+#  setnames(cond_expec_dt_new, "V1", "cond_expec")
 
   feat_names <- colnames(explainer$x_train)
   dim <- length(feat_names)
 
-
   cond_expec_list <- list()
   cond_expec_list[[1]] <- NULL
 
-  # NOTE: Could speed up this by adding a group to each unique conditioning combination in cond_list
-  # (should be done where cond_list is created, call this group1), and then do the prediction and just once beforehand,
-  # merge that with an rbindlist of cond_list based on features, compute hte expected_value like below
-  # and do the sum(expected_value) by (group1,group_id_from_rbindlist_of_cond_list)
-  # Currently this takes 49 seconds  seconds for dim 10 --
-  # Priority 2
-  start = proc.time()
+  joint_prob_dt[,predict := predict_model(explainer$model, newdata = .SD), .SDcols = feat_names]
+
   for(i in 2:nrow(explainer$S)){
     col_names <- feat_names[as.logical(explainer$S[i, ])]
-    cond_list[[i]][, predict := predict_model(explainer$model, newdata = .SD), .SDcols = feat_names]
+    #cond_list[[i]][, predict := predict_model(explainer$model, newdata = .SD), .SDcols = feat_names]
+    cond_list[[i]] <- merge(cond_list[[i]],joint_prob_dt[,.(feat_comb_id,predict)],by="feat_comb_id") # 50 sec-> 30 sec in for dim 10
     cond_list[[i]][, expected_value := predict * cond_prob]
-    tmp <- cond_list[[i]][, sum(expected_value), by = col_names]
-    # tmp[, conditioned_on := paste(col_names, collapse = ", ")]
-    cond_expec_list[[i]] <- tmp
+    cond_expec_list[[i]] <- cond_list[[i]][, sum(expected_value), by = col_names]
   }
   cond_expec_dt <- rbindlist(l = cond_expec_list, use.names = T,fill = TRUE,)
   setnames(cond_expec_dt, "V1", "cond_expec")
   setcolorder(cond_expec_dt, c(feat_names, "cond_expec")) # "conditioned_on"
-
-  end = proc.time()
-  end-start
 
   S_dt <- data.table(explainer$S)
   S_dt[, id := 0:(nrow(S_dt) - 1)]
@@ -54,124 +52,24 @@ cond_expec_new <- function(cond_list,explainer,x_test, prediction_zero){
 
   mat <- unique(x_test)
   mat <- mat[, lapply(.SD, as.factor), .SDcol = feat_names] # To be removed later
-  setkey(mat) # Just to simplify now, remove later
+#  setkey(mat) # Just to simplify now, remove later
   mat[,rowid:=.I]
 
   cond_expec_dt[, colnum := col_fun(.SD, S_dt), .SDcol = feat_names]
 
-
   # Check if it is possible to do the join the other way around
   select_cols <- c(feat_names, "i.cond_expec", "i.colnum")
   tmp <- list()
-### MJ START TESTING ####
-  tmp1 <- list()
-
-  start <- proc.time()
   for(i in 2:nrow(explainer$S)){
     col_names <- feat_names[as.logical(explainer$S[i, ])]
     cond_expec_dt_sub <- subset(cond_expec_dt,subset = colnum==i-1)
-
-    tmp1[[i]] <- mat[cond_expec_dt_sub, .(rowid,i.cond_expec,i.colnum), on = col_names,by=.EACHI]
+    tmp[[i]] <- cond_expec_dt_sub[mat, .(rowid,cond_expec,colnum), on = col_names,allow.cartesian=TRUE]
   }
-  end <- proc.time()
-  end-start
+  tmp_dt <- rbindlist(tmp,use.names = T)
 
-  tmp2 <- list()
-  start <- proc.time()
-  for(i in 2:nrow(explainer$S)){
-    col_names <- feat_names[as.logical(explainer$S[i, ])]
-    cond_expec_dt_sub <- subset(cond_expec_dt,subset = colnum==i-1)
-
-    tmp2[[i]] <- cond_expec_dt_sub[mat, .(rowid,cond_expec,colnum), on = col_names,by=.EACHI]
-  }
-  end <- proc.time()
-  end-start
-
-  tmp3 <- list()
-  start <- proc.time()
-  for(i in 2:nrow(explainer$S)){
-    col_names <- feat_names[as.logical(explainer$S[i, ])]
-    cond_expec_dt_sub <- subset(cond_expec_dt,subset = colnum==i-1)
-
-    tmp3[[i]] <- cond_expec_dt_sub[mat, .(rowid,cond_expec,colnum), on = col_names,allow.cartesian=TRUE]
-  }
-  end <- proc.time()
-  end-start
-
-  tmp_dt <- rbindlist(tmp3,use.names = T)
   final_dt <- dcast(tmp_dt, formula = "rowid~colnum", value.var = "cond_expec")
-  x_test_id <- mat[x_test,]
+  x_test_id <- mat[x_test, on = feat_names]
   S_char_vec <- as.character(1:(nrow(explainer$S)-1))
-
   final_dt_x_test <- cbind("0"=prediction_zero, final_dt[x_test_id,..S_char_vec,on="rowid"])
-
-
-
-  tmp_dt <- rbindlist(tmp)
-  tmp_dt <- na.omit(tmp_dt)
-  tmp_dt <- merge(mat,tmp_dt,all.x=T,all.y=F)
-
-  setnames(tmp_dt, c("i.cond_expec","i.colnum"), c("cond_expec","colnum"))
-
-  final_dt <- dcast(tmp_dt, formula = paste0(paste0(feat_names, collapse = "+"), "~colnum"), value.var = "cond_expec")
-
-  d
-
-  for(i in 2:nrow(explainer$S)){
-    col_names <- feat_names[as.logical(explainer$S[i, ])]
-    cond_expec_dt_sub <- subset(cond_expec_dt,subset = colnum==i-1)
-
-    tmp[[i]] <- mat[cond_expec_dt_sub, .(rowid,i.cond_expec,i.colnum), on = col_names,by=.EACHI]
-  }
-
-
-
-    aa[rowid==1,]
-
-    bb <- cond_expec_dt_sub[mat, .(rowid,cond_expec,colnum), on = col_names,by=.EACHI]
-    bb[rowid==1,]
-
-    cc <- cond_expec_dt_sub[mat, .(rowid,cond_expec,colnum), on = col_names,allow.cartesian=TRUE]
-    cc[rowid==1,]
-
-
-    for(i in 2:nrow(explainer$S)){
-    col_names <- feat_names[as.logical(explainer$S[i, ])]
-    cond_expec_dt[mat, .(i.feat_1_,i.feat_2_,i.feat_3_,i.feat_4_,cond_expec,colnum), on = col_names,by=.EACHI]
-    aa=mat[cond_expec_dt, .(feat_1_,feat_2_,feat_3_,feat_4_,i.cond_expec,i.colnum), on = col_names,by=.EACHI]
-
-
-    cond_expec_dt_sub <- subset(cond_expec_dt,subset = colnum==i-1)
-
-    aa=mat[cond_expec_dt_sub, .(rowid,i.cond_expec,i.colnum), on = col_names,by=.EACHI]
-    aa[rowid==1,]
-
-    bb <- cond_expec_dt_sub[mat, .(rowid,cond_expec,colnum), on = col_names,by=.EACHI]
-    bb[rowid==1,]
-
-    cc <- cond_expec_dt_sub[mat, .(rowid,cond_expec,colnum), on = col_names,allow.cartesian=TRUE]
-    cc[rowid==1,]
-
-
-# MJ END TESTING ####
-
-
-    for (i in 1:nrow(cond_expec_dt)){ # THIS IS VERY SLOW FOR LARGE DIMS (BIG LOOP), CAN WE DO SOMETHING?
-    on_cols <- feat_names[!is.na(subset(cond_expec_dt[i,], select = feat_names))]
-    tmp[[i]] <- mat[cond_expec_dt[i, ], ..select_cols, on = on_cols]
-  }
-
-  # Faster ways to do the reduction below here faster. Very fast anyway... Do not prioritize
-  tmp_dt <- rbindlist(tmp)
-  tmp_dt <- na.omit(tmp_dt)
-  tmp_dt <- merge(mat,tmp_dt,all.x=T,all.y=F)
-
-  setnames(tmp_dt, c("i.cond_expec","i.colnum"), c("cond_expec","colnum"))
-
-  final_dt <- dcast(tmp_dt, formula = paste0(paste0(feat_names, collapse = "+"), "~colnum"), value.var = "cond_expec")
-
-  results_dt <- merge(x_test,final_dt,by=feat_names,sort=F)
-
-  results_dt <- cbind("0"=prediction_zero,results_dt[,-(1:dim)])
-  return(results_dt)
+  return(final_dt_x_test)
 }
