da453a9ff1ed5b4ed4cd5ff6fa9ac18f3826e09e
Martin Jullum <jullum@nr.no>
2020-02-26 15:36:22 +0100

    Fixes kernelSHAP issues
    Checked once to give almost identical results to empirical_ind using one-hot encoding

--- inst/devel_scripts/paper_simulations/calculate_true_shapley_withdatatable_KernelSHAP.R
+++ inst/devel_scripts/paper_simulations/calculate_true_shapley_withdatatable_KernelSHAP.R
@@ -15,29 +15,29 @@ getstr = function(mystring, initial.character = "_", final.character = "_") {
   if (!is.character(mystring)) {
     stop('The parent string must be a character variable.')
   }
-  
+
   if (!is.character(initial.character)) {
     stop('The initial character must be a character variable.')
   }
-  
-  
+
+
   if (!is.character(final.character)) {
     stop('The final character must be a character variable.')
   }
-  
+
   add = 0
   if(initial.character == final.character){add=1}
-  
+
   # pre-allocate a vector to store the extracted strings
   snippet = rep(0, length(mystring))
-  
+
   for (i in 1:length(mystring)) {
     # extract the initial position
     initial.position <- gregexpr(initial.character, mystring[i])[[1]][1] + 1
-    
+
     # extract the final position
     final.position <- gregexpr(final.character, mystring[i])[[1]][1+add] - 1
-    
+
     # extract the substring between the initial and final positions, inclusively
     snippet[i] <- substr(mystring[i], initial.position, final.position)
   }
@@ -67,7 +67,7 @@ getstr = function(mystring, initial.character = "_", final.character = "_") {
 #' @export
 
 sim_true_Normal <- function(mu, Sigma, beta, N_shapley = 10000, explainer, cutoff, response_mod){
-  
+
   feat_names <- colnames(explainer$x_train)
   dim <- length(feat_names)
   if(!is.null(dim(cutoff))){
@@ -75,7 +75,7 @@ sim_true_Normal <- function(mu, Sigma, beta, N_shapley = 10000, explainer, cutof
     cutoff[, 4] <- cutoff[, 4] + 10
   }
   no_categories <- length(cutoff) -1
-  
+
   set.seed(1)
   sim <- mvrnorm(n = N_shapley, mu = mu, Sigma = Sigma)
   dt <- NULL
@@ -90,38 +90,38 @@ sim_true_Normal <- function(mu, Sigma, beta, N_shapley = 10000, explainer, cutof
   }
   dt <- data.table(dt)
   setnames(dt, names(dt), paste0("feat_", 1:dim,"_"))
-  
+
   ## this is for the list being returned
   prop <- NULL
   for(i in 1:dim){
     prop <- c(prop, table(dt[, feat_names[i], with = FALSE]) / N_shapley)
   }
-  
+
   ## everything below here is to get the mean of the responses i.e 'mn' -------
   dt <- dt[, lapply(.SD, as.factor)]
-  
+
   dt[, 'epsilon' := 0]
-  
+
   mod_matrix <- model.matrix(~.-1, data = dt[, 1:dim],
                              contrasts.arg = lapply(dt[, 1:dim],contrasts,contrasts=FALSE))
   dt <- cbind(dt, data.table(mod_matrix))
-  
+
   ## 3. Calculate response
   all_responses <- response_mod(mod_matrix_full = cbind(1,mod_matrix),
                                 beta = beta,
                                 epsilon = dt$epsilon)
   mn <- mean(all_responses)
-  
+
   ## -------
-  
+
   joint_prob <- table(dt[, ..feat_names])  / N_shapley
   joint_prob_dt0 <- data.table(joint_prob)
   joint_prob_dt <- joint_prob_dt0[, lapply(.SD, as.factor), .SDcols = feat_names]
   joint_prob_dt <- cbind(joint_prob_dt, joint_prob_dt0[, .(N)])
   setnames(joint_prob_dt,"N", "joint_prob")
-  
+
   joint_prob_dt[, feat_comb_id:=.I]
-  
+
   return(list(joint_prob_dt, mn, prop))
 }
 
@@ -137,17 +137,17 @@ sim_true_Normal <- function(mu, Sigma, beta, N_shapley = 10000, explainer, cutof
 #' @export
 
 marg_prob <- function(joint_prob_dt, explainer){
-  
+
   feat_names <- colnames(explainer$x_train)
-  
+
   ## compute all marginal probabilities
   marg_list <- list()
   marg_list[[1]] <- NA
   for(i in 2:nrow(explainer$S)){
     col_names <- feat_names[as.logical(explainer$S[i, ])]
-    
+
     mat <- joint_prob_dt[, .(marg_prob = sum(joint_prob)), by = col_names]
-    
+
     marg_list[[i]] <- mat
   }
   return(marg_list)
@@ -166,25 +166,25 @@ marg_prob <- function(joint_prob_dt, explainer){
 #' @export
 
 cond_prob <- function(marg_list, joint_prob_dt, explainer){
-  
+
   feat_names <- colnames(explainer$x_train)
-  
+
   cond_list <- list()
   cond_list[[1]] <- NA
-  
+
   for(i in 2:nrow(explainer$S)){
     col_names <- feat_names[as.logical(explainer$S[i, ])]
-    
+
     mat0 <- marg_list[[i]]
     setkeyv(mat0, col_names)
     setkeyv(joint_prob_dt, col_names)
     mat <- merge(mat0, joint_prob_dt, all.x = TRUE)
     mat[, cond_prob := joint_prob / marg_prob]
     #mat[, conditioned_on := paste(col_names, collapse = ", ")]
-    
+
     cond_list[[i]] <- mat
   }
-  
+
   return(cond_list)
 }
 
@@ -227,23 +227,23 @@ col_fun <- function(tbl, S_dt){
 #' @export
 
 cond_expec_new <- function(cond_list, explainer, x_test, prediction_zero, joint_prob_dt){
-  
+
   feat_names <- colnames(explainer$x_train)
   dim <- length(feat_names)
-  
+
   S_dt <- data.table(explainer$S)
   S_dt[, id := 0:(nrow(S_dt) - 1)]
   setnames(S_dt, c(feat_names, "id"))
-  
+
   mat <- unique(x_test)
   mat <- mat[, lapply(.SD, as.factor), .SDcol = feat_names] # To be removed later
   mat[, rowid := .I] # Adding identifyer to match on
-  
+
   cond_expec_list <- list()
   cond_expec_list[[1]] <- NULL
-  
+
   joint_prob_dt[, predict := predict_model(explainer$model, newdata = .SD), .SDcols = feat_names]
-  
+
   tmp <- NULL
   for(i in 2:nrow(explainer$S)){
     col_names <- feat_names[as.logical(explainer$S[i, ])]
@@ -254,12 +254,12 @@ cond_expec_new <- function(cond_list, explainer, x_test, prediction_zero, joint_
                       colnum = i - 1)
   }
   tmp_dt <- rbindlist(tmp,use.names = T)
-  
+
   final_dt <- dcast(tmp_dt, formula = "rowid~colnum", value.var = "cond_expec")
   x_test_id <- mat[x_test, on = feat_names]
   S_char_vec <- as.character(1:(nrow(explainer$S) - 1))
   final_dt_x_test <- cbind("0" = prediction_zero, final_dt[x_test_id, ..S_char_vec,on = "rowid"])
-  
+
   return(final_dt_x_test)
 }
 
@@ -284,7 +284,7 @@ true_Kshap <- function(explainer, cond_expec_mat, x_test){
   }
   Kshap <- data.table(Kshap)
   setnames(Kshap, 1:(dim + 1), c("none", names(x_test)))
-  
+
   return(Kshap)
 }
 
@@ -302,12 +302,12 @@ true_Kshap <- function(explainer, cond_expec_mat, x_test){
 #' @export
 
 linear_Kshap <- function(x_test_onehot_full, beta, prop){
-  
+
   beta_matcher <- as.numeric(getstr(colnames(x_test_onehot_full)))
   no_features <- max(beta_matcher)
-  
+
   phi0 <- beta[1] + sum(beta[-1] * prop)
-  
+
   mult <- (t(x_test_onehot_full) - matrix(prop, nrow = length(prop), ncol = nrow(x_test_onehot_full)))
   phi_raw <- t(mult*beta[-1])
   phi <- matrix(NA, nrow = nrow(phi_raw), ncol = no_features)
@@ -316,7 +316,7 @@ linear_Kshap <- function(x_test_onehot_full, beta, prop){
   }
   phi <- cbind(phi0,phi)
   colnames(phi)[-1] <- paste0("phi", 1:no_features)
-  
+
   return(phi)
 }
 
@@ -366,9 +366,9 @@ AE <- function(true_shapley, shapley_method){
 
 
 simulate_data <- function(parameters_list){
-  
+
   timeit <- list()
-  
+
   #
   mu <- parameters_list$mu
   beta <- parameters_list$beta
@@ -386,8 +386,8 @@ simulate_data <- function(parameters_list){
   no_categories <- parameters_list$no_categories
   if(is.null(seed)) seed <- 1
   dim <- length(mu)
-  
-  
+
+
   # check correct input
   if(!all(is.numeric(mu))){
     stop("mu vector must contain only numerics.")
@@ -417,7 +417,7 @@ simulate_data <- function(parameters_list){
       stop("cutoff vector must either be length of no_categories plus 1 or be NULL.")
     }
   }
-  
+
   ## make sure Sigma is positive definite
   Sigma <- matrix(rep(parameters_list$corr, dim^2), dim, dim)
   for(i in 1:dim){
@@ -429,17 +429,17 @@ simulate_data <- function(parameters_list){
     print("New Sigma matrix:")
     print(Sigma)
   }
-  
+
   print(paste0("Dimension: ", dim), quote = FALSE, right = FALSE)
   print(paste0("Number of categories: ", no_categories), quote = FALSE, right = FALSE)
   print(paste0("N_shapley: ", N_shapley), quote = FALSE, right = FALSE)
   print(paste0("No_train_obs: ", No_train_obs), quote = FALSE, right = FALSE)
-  
+
   ## 1. simulate training data
   tm_current <- Sys.time()
   set.seed(seed)
   x <- mvrnorm(n =  No_train_obs, mu = mu, Sigma = Sigma)
-  
+
   dt <- NULL
   if(is.null(cutoff)){ ## to get equal proportion in each level
     for(i in 1:no_categories){
@@ -452,32 +452,32 @@ simulate_data <- function(parameters_list){
       dt <- cbind(dt, cut(x[, i], cutoff, labels = 1:no_categories))
     }
   }
-  
-  
+
+
   ## 1.2 Get every combination of the levels and dimensions
   x_test_list <- list()
   for(i in 1:dim){
     x_test_list[[i]] <- 1:no_categories
   }
   x_test_dt <- do.call(CJ, x_test_list)
-  
+
   if(Sample_test){
     if(nrow(x_test_dt) > No_test_sample){
       sampled_rows <- sample(1:nrow(x_test_dt), size = No_test_sample, replace = FALSE)
       x_test_dt <- x_test_dt[sampled_rows, ]
     }
   }
-  
+
   No_test_obs <- nrow(x_test_dt)
   dt <- rbind(dt, x_test_dt)
-  
+
   dt <- data.table(dt)
   setnames(dt, names(dt), paste0("feat_", 1:dim,"_"))
   feat_names <- names(dt[, 1:dim])
-  
+
   dt_numeric <- dt
   dt <- dt[, lapply(.SD, as.factor)]
-  
+
   if(noise == TRUE){
     dt_numeric[, epsilon := rnorm(No_test_obs + No_train_obs, 0, 0.1^2)] #
     dt[, epsilon := rnorm(No_test_obs + No_train_obs, 0, 0.1^2)] #
@@ -485,114 +485,116 @@ simulate_data <- function(parameters_list){
     dt_numeric[, epsilon := 0]
     dt[, epsilon := 0]
   }
-  
+
   ## 2. One-hot encoding of training data
   mod_matrix <- model.matrix(~.-1, data = dt[, 1:dim], contrasts.arg = lapply(dt[, 1:dim], contrasts, contrasts = FALSE))
-  
+
   dt <- cbind(dt, data.table(mod_matrix))
   full_onehot_names <- colnames(mod_matrix)
   reduced_onehot_names <- full_onehot_names[-grep("_1$", full_onehot_names)] # names without reference levels
-  
+
   ## 3. Calculate response
   dt[, response := response_mod(mod_matrix_full = cbind(1, mod_matrix), beta = beta, epsilon = epsilon)]
   dt_numeric[, response := dt[['response']]]
-  
-  
+
+
   ## 4. Fit model
   if(fit_mod == 'regression'){
     form <- as.formula(paste0("response~", paste(feat_names, collapse = "+")))
     model <- lm(formula = form, data = dt[(1:No_train_obs), ])
-    
+
     fmla_onehot <- as.formula(paste("response ~", paste(reduced_onehot_names, collapse = " + ")))
     model_onehot <- lm(fmla_onehot, data = dt[(1:No_train_obs)])
   }
-  
+
   ## 5. initalize shapr object with trained model -- this is used for calculating true shapley
   x_train <- dt[(1:No_train_obs), ..feat_names] ## used in explainer()
   x_test <- dt[-(1:No_train_obs), ..feat_names] ## used in cond_expec_mat()
   y_train <- dt[(1:No_train_obs), .(response)] ## used in cond_expec_mat()
-  
+
   x_train_numeric <- dt_numeric[(1:No_train_obs), ..feat_names] ## used in explainer()
   x_test_numeric <- dt_numeric[-(1:No_train_obs), ..feat_names] ## used in cond_expec_mat()
 
   # For computing the true Shapley values (with correlation 0)
   x_test_onehot_full <- dt[-(1:No_train_obs), ..full_onehot_names]
-  
+
   x_test_onehot_reduced <- dt[-(1:No_train_obs), ..reduced_onehot_names]
   x_train_onehot_reduced <- dt[(1:No_train_obs), ..reduced_onehot_names]
-  
+
   ##
   explainer <- shapr(x_train, model) # print(class(model)) # "lm"
   # tmp0 <- predict_model(model, x_test)
-  
+
   ## NEW
   # Create custom function of model_type for lm
   model_type.numeric_lm <<- function(x) {
   }
-  
+
+  features.numeric_lm <<- function(x, cnms, feature_labels = NULL) {
+    if (!is.null(feature_labels)) message_features_labels()
+
+    nms <- tail(all.vars(x$terms), -1)
+    if (!all(nms %in% cnms)) error_feature_labels()
+
+    return(nms)
+  }
+
+
   # Create custom function of predict_model for caret
   predict_model.numeric_lm <<- function(x, newdata) {
-    # print(str(newdata))
-    print("Hello")
+    newdata <- as.data.table(newdata)
     newdata0 <- newdata[, lapply(.SD, as.factor)]
-    
-    # feat_1_ <- factor(newdata[[1]])
-    # feat_2_ <- factor(newdata[[2]])
-    # feat_3_ <- factor(newdata[[3]])
-    # newdata0 <- data.frame(feat_1_, feat_2_, feat_3_)
-    # 
+
     class(x) <- "lm"
-    print("HERE")
     predict(x, newdata0)
-    print("HERE2")
   }
   class(model) <- "numeric_lm"
-  
+
   explainer_numeric <- shapr(x_train_numeric, model)
   ## END
-  
+
   ##
   # tmp <- predict_model(model, x_test_numeric)
-  
-  
+
+
   if(!all(methods == 'ctree')){
     explainer_onehot <- shapr(x_train_onehot_reduced, model_onehot)
   }
   ##
-  
+
   ## 6. calculate the true shapley values
   tm_true_Shapley <- Sys.time();
   print("Started to calculate true Shapley values.", quote = FALSE, right = FALSE)
-  
+
   tm0 <- proc.time();
   joint_prob_dt_list <- sim_true_Normal(mu, Sigma, beta, N_shapley = N_shapley, explainer, cutoff, response_mod)
   tm1 <- proc.time();
   timeit['Simulate_true_Normal'] <- list((tm1 - tm0))
-  
+
   tm0 <- proc.time();
   marg_list <- marg_prob(joint_prob_dt_list[[1]], explainer)
   tm1 <- proc.time();
   timeit['Calculate_marginal_distributions'] <- list((tm1 - tm0))
-  
+
   tm0 <- proc.time();
   cond_list <- cond_prob(marg_list, joint_prob_dt_list[[1]], explainer)
   tm1 <- proc.time();
   timeit['Calculate_conditional_distributions'] <- list((tm1 - tm0))
-  
+
   tm0 <- proc.time();
   cond_expec_mat <- cond_expec_new(cond_list, explainer, x_test, prediction_zero = joint_prob_dt_list[[2]], joint_prob_dt = joint_prob_dt_list[[1]])
   tm1 <- proc.time();
   timeit['Calculate_expectations_distributions'] <- list((tm1 - tm0))
-  
+
   tm0 <- proc.time();
   true_shapley <- true_Kshap(explainer, cond_expec_mat, x_test)
   tm1 <- proc.time();
   timeit['Calculate_true_Shapley'] <- list((tm1 - tm0))
-  
+
   tm_true_Shapley1 <- Sys.time();
   print("Finished calculating true Shapley values.", quote = FALSE, right = FALSE)
   print(tm_true_Shapley1 - tm_true_Shapley)
-  
+
   if(explainer$model_type == 'regression'){
     if(parameters_list$corr == 0){
       true_linear <-linear_Kshap(x_test_onehot_full = x_test_onehot_full, beta = beta, prop = joint_prob_dt_list[[3]])
@@ -602,20 +604,20 @@ simulate_data <- function(parameters_list){
   } else{
     true_linear <- NULL
   }
-  
+
   ## 8. calculate approximate shapley value with different methods
   p <- mean(y_train$response) # since y_train is no longer a matrix
-  
+
   ## to compute sum of Shapley values - only for one-hot encoded variables
   beta_matcher <- as.numeric(getstr(reduced_onehot_names))
   no_features <- max(beta_matcher)
   phi_sum_mat <- matrix(NA, nrow = No_test_obs, ncol = no_features)
-  
-  
+
+
   tm_now <- proc.time();
   print("Started to estimate Shapley values with various methods.", quote = FALSE, right = FALSE)
-  
-  
+
+
   explanation_list <- list()
   for(m in methods){
     if(m == 'empirical_ind'){
@@ -629,17 +631,17 @@ simulate_data <- function(parameters_list){
         sample = FALSE,
         n_samples = 1000)
       tm1 <- proc.time()
-      
+
       for (i in 1:no_features){
         phi_sum_mat[, i] <- rowSums(subset(explanation_list[[m]]$dt, select = which(beta_matcher == i) + 1))
       }
       colnames(phi_sum_mat) <- feat_names
       explanation_list[[m]]$dt_sum <- cbind(explanation_list[[m]]$dt[, 1], phi_sum_mat)
-      
+
       print(paste0("Finished estimating Shapley value with ", m, " method."), quote = FALSE, right = FALSE)
       print(tm1 - tm0)
       timeit[m] <- list((tm1 - tm0))
-      
+
     } else if(m == 'empirical'){
       tm0 <- proc.time()
       explanation_list[[m]] <- explain(
@@ -651,17 +653,17 @@ simulate_data <- function(parameters_list){
         w_threshold = 1,
         n_samples = 1000)
       tm1 <- proc.time()
-      
+
       for (i in 1:no_features){
         phi_sum_mat[, i] <- rowSums(subset(explanation_list[[m]]$dt, select = which(beta_matcher == i) + 1))
       }
       colnames(phi_sum_mat) <- feat_names
       explanation_list[[m]]$dt_sum <- cbind(explanation_list[[m]]$dt[, 1], phi_sum_mat)
-      
+
       print(paste0("Finished estimating Shapley value with ", m, " method."), quote = FALSE, right = FALSE)
       print(tm1 - tm0)
       timeit[m] <- list((tm1 - tm0))
-      
+
     } else if(m == 'gaussian'){
       for(j in N_sample_gaussian){
         tm0 <- proc.time()
@@ -674,18 +676,18 @@ simulate_data <- function(parameters_list){
           w_threshold = 1,
           n_samples = j)
         tm1 <- proc.time()
-        
-        
+
+
         for (i in 1:no_features){
           phi_sum_mat[, i] <- rowSums(subset(explanation_list[[paste0(m, "_nsamples", j)]]$dt, select = which(beta_matcher == i) + 1))
         }
         colnames(phi_sum_mat) <- feat_names
         explanation_list[[paste0(m, "_nsamples", j)]]$dt_sum <- cbind(explanation_list[[paste0(m, "_nsamples", j)]]$dt[, 1], phi_sum_mat)
-        
+
         print(paste0("Finished estimating Shapley value with ", paste0(m, "_nsamples", j), " method."), quote = FALSE, right = FALSE)
         print(tm1 - tm0)
         timeit[paste0(m, "_nsamples", j)] <- list((tm1 - tm0))
-        
+
       }
     } else if(m == 'ctree_onehot'){
       tm0 <- proc.time()
@@ -697,17 +699,17 @@ simulate_data <- function(parameters_list){
         sample = FALSE,
         mincriterion = 0.95)
       tm1 <- proc.time()
-      
+
       for (i in 1:no_features){
         phi_sum_mat[, i] <- rowSums(subset(explanation_list[[m]]$dt, select = which(beta_matcher == i) + 1))
       }
       colnames(phi_sum_mat) <- feat_names
       explanation_list[[m]]$dt_sum <- cbind(explanation_list[[m]]$dt[, 1], phi_sum_mat)
-      
+
       print(paste0("Finished estimating Shapley value with ", m, " method."), quote = FALSE, right = FALSE)
       print(tm1 - tm0)
       timeit[m] <- list((tm1 - tm0))
-      
+
     } else if(m == 'ctree'){
       tm0 <- proc.time()
       explanation_list[[m]] <- explain(
@@ -722,7 +724,7 @@ simulate_data <- function(parameters_list){
       print(paste0("Finished estimating Shapley value with ", m, " method."), quote = FALSE, right = FALSE)
       print(tm1 - tm0)
       timeit[m] <- list((tm1 - tm0))
-  
+
     }
     else if(m == 'kernelSHAP'){
       tm0 <- proc.time()
@@ -739,15 +741,15 @@ simulate_data <- function(parameters_list){
       print(paste0("Finished estimating Shapley value with kernelSHAP method."), quote = FALSE, right = FALSE)
       print(tm1 - tm0)
       timeit['kernelSHAP'] <- list((tm1 - tm0))
-      
+
     }
   }
   ## for kernelSHAP
-  
+
   tm_now1 <- proc.time();
   print("Ended estimating Shapley values with various methods.", quote = FALSE, right = FALSE)
-  
-  
+
+
   return_list <- list()
   return_list[['true_shapley']] <- true_shapley
   return_list[['true_linear']] <- true_linear
@@ -758,6 +760,6 @@ simulate_data <- function(parameters_list){
   return_list[['parameters']] <- parameters_list
   print("--- End ---")
   return(return_list)
-  
+
 }
 
