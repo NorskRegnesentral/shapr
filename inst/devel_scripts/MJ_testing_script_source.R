000584a09b67669ea6e823399fa2f52809251c84
Martin Jullum <jullum@nr.no>
2020-03-02 12:54:09 +0100

    testing

---
+++ inst/devel_scripts/MJ_testing_script_source.R
@@ -0,0 +1,13 @@
+
+a = 12
+modeltools::fit(party::conditionalTree, ls, controls = controls, weights = weights)
+
+func <- function(){
+  modeltools::fit(party::conditionalTree, ls, controls = controls, weights = weights)
+}
+
+#bb = function(){
+#  aa = func()
+#}
+
+
