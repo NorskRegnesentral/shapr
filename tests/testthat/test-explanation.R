0167674b56563e39e0ce72b226f61adce4ae0afe
Martin Jullum <jullum@nr.no>
2020-03-23 20:33:06 +0100

    Fixes for R-devel (#199)

--- tests/testthat/test-explanation.R
+++ tests/testthat/test-explanation.R
@@ -4,7 +4,7 @@ library(shapr)
 context("test-explanation.R")
 
 # For using same Random numer generator as CircelCI (R version 3.5.x)
-if (as.numeric(version$minor) >= 6.0) RNGkind(sample.kind = "Rounding")
+RNGversion(vstr = "3.5.0")
 
 test_that("Test functions in explanation.R", {
 
