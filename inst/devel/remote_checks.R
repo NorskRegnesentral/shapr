bfc71e4dcfd343f14fcb4734a51244f3f2371f3b
Martin Jullum <jullum@nr.no>
2020-08-05 23:23:33 +0200

    script with remote checks

---
+++ inst/devel/remote_checks.R
@@ -0,0 +1,24 @@
+
+# rcmdcheck
+
+
+install.packages("rcmdcheck")
+
+library(rcmdcheck)
+rcmdcheck(path = getwd())
+
+
+# Run package on Winbuilder
+
+devtools::check_win_devel()
+devtools::check_win_oldrelease()
+devtools::check_win_release()
+
+# Check pacakge on Rhub
+
+#install.packages("rhub")
+
+#rhub::validate_email(email="Martin.Jullum@nr.no")
+
+devtools::check_rhub()
+
