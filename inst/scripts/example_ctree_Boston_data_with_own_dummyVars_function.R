a01b19a15488e8bcfb6db4193e3e88aefe0930e0
Annabelle Redelmeier <aredelmeier@gmail.com>
2020-08-07 11:33:15 +0200

    clean up

--- inst/scripts/example_ctree_Boston_data_with_own_dummyVars_function.R
+++ inst/scripts/example_ctree_Boston_data_with_own_dummyVars_function.R
@@ -1,7 +1,8 @@
 library(xgboost)
 library(shapr)
 
-## ----------------------------
+source("inst/scripts/make_dummies_no_class.R")
+
 data("Boston", package = "MASS")
 
 x_var <- c("lstat", "chas", "rad", "indus")
@@ -15,38 +16,56 @@ x_train <- Boston[-1:-6, x_var]
 y_train <- Boston[-1:-6, y_var]
 x_test <- Boston[1:6, x_var]
 
-# library(caret)
-# # -- special function when using categorical data + xgboost
-# dummyfunc <- caret::dummyVars(" ~ .", data = rbind(x_train, x_test))
-# x_train_dummy <- predict(dummyfunc, newdata = x_train)
-# x_test_dummy <- predict(dummyfunc, newdata = x_test)
-#
-# model_cat <- xgboost(
-#   data = x_train_dummy,
+dummyfunc_original <- make_dummies(data = rbind(x_train, x_test))
+
+x_train_dummies <- apply_dummies(obj = dummyfunc_original, newdata = x_train)
+x_test_dummies <- apply_dummies(obj = dummyfunc_original, newdata = x_test)
+
+# model_cont <- xgboost(
+#   data = as.matrix(x_train),
 #   label = y_train,
 #   nround = 20,
 #   verbose = FALSE
 # )
-#
-# model_cat$dummyfunc <- dummyfunc
-#
-# explainer_cat <- shapr(x_train, model_cat)
-#
-# p <- mean(y_train)
-#
-# explanation_ctree <- explain(
-#   x_test,
-#   approach = "ctree",
-#   explainer = explainer_cat,
-#   prediction_zero = p
-# )
+# explainer_cont <- shapr(x_train, model_cont)
 
+model_cat <- xgboost(
+  data = x_train_dummies,
+  label = y_train,
+  nround = 20,
+  verbose = FALSE
+)
 
-## NEW
-source("inst/scripts/make_dummies.R")
+model_cat$dummylist <- dummyfunc_original # this is very important! and must be called dummylist to work with the R code!
+explainer_cat <- shapr(x_train, model_cat)
+
+p <- mean(y_train)
+
+explanation_ctree <- explain(
+  x_test,
+  approach = "ctree",
+  explainer = explainer_cat,
+  prediction_zero = p
+)
+
+## TESTS
+source("inst/scripts/make_dummies_no_class.R")
+
+data("Boston", package = "MASS")
+
+x_var <- c("lstat", "chas", "rad", "indus")
+y_var <- "medv"
+
+# convert to factors
+Boston$rad = as.factor(Boston$rad)
+Boston$chas = as.factor(Boston$chas)
+
+x_train <- Boston[-1:-6, x_var]
+y_train <- Boston[-1:-6, y_var]
+x_test <- Boston[1:6, x_var]
+
 dummyfunc_original <- make_dummies(data = rbind(x_train, x_test))
 
-## Tests
 head(x_train)
 original <- predict(dummyfunc_original, newdata = x_train)
 head(original)
@@ -99,57 +118,28 @@ colnames(x_train3) <- c("", "", "", "")
 head(x_train3)
 dummyfunc_no_var_names <- make_dummies(data = rbind(x_train3)) # will throw an error
 
-
-## RUN THROUGH
-library(shapr)
-library(xgboost)
-source("inst/scripts/make_dummies_no_class.R")
-
-data("Boston", package = "MASS")
-
-x_var <- c("lstat", "chas", "rad", "indus")
-y_var <- "medv"
-
-# convert to factors
-Boston$rad = as.factor(Boston$rad)
-Boston$chas = as.factor(Boston$chas)
-
-x_train <- Boston[-1:-6, x_var]
-y_train <- Boston[-1:-6, y_var]
-x_test <- Boston[1:6, x_var]
-
-dummyfunc_original <- make_dummies(data = rbind(x_train, x_test))
-
-##
-x_train_dummies <- apply_dummies(obj = dummyfunc_original, newdata = x_train)
-x_test_dummies <- apply_dummies(obj = dummyfunc_original, newdata = x_test)
-
-# model_cont <- xgboost(
-#   data = as.matrix(x_train),
+# library(caret)
+# # -- special function when using categorical data + xgboost
+# dummyfunc <- caret::dummyVars(" ~ .", data = rbind(x_train, x_test))
+# x_train_dummy <- predict(dummyfunc, newdata = x_train)
+# x_test_dummy <- predict(dummyfunc, newdata = x_test)
+#
+# model_cat <- xgboost(
+#   data = x_train_dummy,
 #   label = y_train,
 #   nround = 20,
 #   verbose = FALSE
 # )
-# explainer_cont <- shapr(x_train, model_cont)
-
-
-model_cat <- xgboost(
-  data = x_train_dummies,
-  label = y_train,
-  nround = 20,
-  verbose = FALSE
-)
-
-model_cat$dummylist <- dummyfunc_original
-explainer_cat <- shapr(x_train, model_cat)
-
-
-
-p <- mean(y_train)
-
-explanation_ctree <- explain(
-  x_test,
-  approach = "ctree",
-  explainer = explainer_cat,
-  prediction_zero = p
-)
+#
+# model_cat$dummyfunc <- dummyfunc
+#
+# explainer_cat <- shapr(x_train, model_cat)
+#
+# p <- mean(y_train)
+#
+# explanation_ctree <- explain(
+#   x_test,
+#   approach = "ctree",
+#   explainer = explainer_cat,
+#   prediction_zero = p
+# )
