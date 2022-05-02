04575a54bfd9f0300f0c3cd38c431cb336e92f17
Martin <jullum@nr.no>
2022-05-02 19:07:27 +0200

    added loop over batches

--- R/explain.R
+++ R/explain.R
@@ -1,16 +1,103 @@
 
 #' @export
+compute_vS <- function(S,explainer){
+  dt <- batch_prepare_vS(S,explainer)
+  batch_get_preds(dt,S,explainer)
+}
+
+#' @export
+batch_prepare_vS <- function(S,explainer){
+  dt <- prepare_data(explainer, index_features = S) # TODO: Modify prepare_data such that it gives does not give copies of the same observation
+}
+
+#' @export
+batch_get_preds <- function(dt,S,explainer){
+  compute_preds(dt, explainer)
+  r_batch_i$dt_mat[, row_id := S]
+  r_batch_i
+}
+
+get_p <- function(dt_vS,explainer){# TODO: Add max_id_comb to explainer to be extracted and used here
+
+  max_id_combination <- explainer$X[,max(id_combination)]
+  p <- unlist(dt_vS[id_combination==max_id_combination,][,id_combination:=NULL])
+
+  p
+
+}
+
+compute_preds <- function(dt, explainer) {
+
+  id_combination <- p_hat <- NULL # due to NSE notes in R CMD check
+
+  # Setup
+  feature_names <- colnames(explainer$x_test)
+
+  # Predictions
+  if (!all(dt[, unique(id_combination)] == 1)) { # Avoid warnings when predicting with empty newdata
+    dt[id_combination != 1, p_hat := predict_model(explainer$model, newdata = .SD), .SDcols = feature_names]
+  }
+  dt[id_combination == 1, p_hat := explainer$prediction_zero]
+
+  return(dt)
+}
+
+compute_MCint <- function(dt, explainer) {
+
+  w <- k <- p_hat <- NULL # due to NSE notes in R CMD check
+
+  # Calculate contributions
+  dt_res <- dt[, .(k = sum((p_hat * w) / sum(w))), .(id, id_combination)]
+  data.table::setkeyv(dt_res, c("id", "id_combination"))
+  dt_mat <- data.table::dcast(dt_res, id_combination ~ id, value.var = "k")
+  #dt_mat[, id_combination := NULL]
+
+  dt_mat
+}
+
+#' @export
 explain_new <- function(x,explainer, approach, prediction_zero,
-                        n_samples = 1e3, n_batches = 1, seed = 1, ...){
+                        n_samples = 1e3, n_batches = 1, seed = 1, keep_samp_for_vS = FALSE, ...){
 
 
-  explain_setup(x = x,
-                explainer = explainer,
-                approach = approach,
-                prediction_zero = prediction_zero,
-                n_samples = n_samples,
-                n_batches = n_batches,
-                seed = seed, ...)
+  explainer <- explain_setup(x = x,
+                             explainer = explainer,
+                             approach = approach,
+                             prediction_zero = prediction_zero,
+                             n_samples = n_samples,
+                             n_batches = n_batches,
+                             seed = seed, ...)
+
+# Starting off with a loop instead of a lapply call
+  dt_vS_list <- list()
+  if(keep_samp_for_vS){
+    dt_samp_for_vS_list <- list()
+  }
+  for(i in seq_along(explainer$S_batch)){
+
+    S <- explainer$S_batch[[i]]
+    dt <- batch_prepare_vS(S = S,explainer = explainer) # Make it optional to store and return the dt_list
+    compute_preds(dt,explainer) # Updating dt by reference
+
+    dt_vS_list[[i]] <- compute_MCint(dt,explainer)
+    if(keep_samp_for_vS){
+      dt_samp_for_vS_list[[i]] <- dt
+    }
+  }
+
+  dt_vS <- rbindlist(dt_vS_list)
+
+  if(keep_samp_for_vS){
+    dt_samp_for_vS <- rbindlist(dt_samp_for_vS_list)
+  }
+
+  p <- get_p(dt_vS,explainer) ### CONT HERE ###
+
+  #r_batch <- future.apply::future_lapply(X = explainer$S_batch,
+  #                                       FUN = compute_vS,
+  #                                       explainer = explainer,
+  #                                       future.seed = explainer$seed)
+
 
   # setup <- explain_setup(x=x,
   #                        explainer = explainer,
@@ -66,7 +153,7 @@ explain_setup <- function(x, explainer, approach, prediction_zero,
   explainer$n_samples <- n_samples
   explainer$n_batches <- n_batches
   explainer$seed <- seed
-  explainer$S_batch <- create_S_batch(explainer, n_batches, NULL)
+  explainer$S_batch <- create_S_batch(explainer, n_batches, NULL) # TODO: Modify create_S_batch such that id_combination=1 is kept outside (and handled elsewhere)
 
   explainer <- setup_approach(explainer, ...)
 
@@ -216,14 +303,34 @@ setup_approach.ctree <- function(explainer,
 
 
 
-#' @export
-batchfun <- function(S,explainer){
-  dt <- prepare_data(explainer, index_features = S, ...)
-  r_batch_i <- prediction(dt, explainer$prediction_zero, explainer)
-  r_batch_i$dt_mat[, row_id := S]
-  r_batch_i
-}
 
+#
+# #  # Check that the number of test observations equals max(id)
+# #  stopifnot(nrow(explainer$x_test) == dt[, max(id)])
+# data.table::setkeyv(dt, c("id", "id_combination"))
+#
+#   # Reducing the prediction data.table
+#   max_id_combination <- nrow(explainer$S)
+#   V1 <- keep <- NULL # due to NSE notes in R CMD check
+#   dt[, keep := TRUE]
+#   first_element <- dt[, tail(.I, 1), .(id, id_combination)][id_combination %in% c(1, max_id_combination), V1]
+#   dt[id_combination %in% c(1, max_id_combination), keep := FALSE]
+#   dt[first_element, c("keep", "w") := list(TRUE, 1.0)]
+#   dt <- dt[keep == TRUE][, keep := NULL]
+#
+#
+#   if (dt[, max(id_combination)] < max_id_combination) {
+#     p_all <- NULL
+#   } else {
+#     p_all <- dt[id_combination == max_id_combination, p_hat]
+#     names(p_all) <- 1:nrow(explainer$x_test)
+#   }
+#
+#
+#
+#   return(r)
+# }
+#
 
 
 #
---
+++ inst/scripts/devel/explain_new.R
@@ -0,0 +1,77 @@
+library(xgboost)
+library(shapr)
+
+data("Boston", package = "MASS")
+
+x_var <- c("lstat", "rm", "dis", "indus")
+y_var <- "medv"
+
+x_train <- as.matrix(Boston[-1:-6, x_var])
+y_train <- Boston[-1:-6, y_var]
+x_test <- as.matrix(Boston[1:6, x_var])
+
+# Looking at the dependence between the features
+cor(x_train)
+
+# Fitting a basic xgboost model to the training data
+model <- xgboost(
+  data = x_train,
+  label = y_train,
+  nround = 20,
+  verbose = FALSE
+)
+
+# Prepare the data for explanation
+explainer <- shapr(x_train, model)
+
+# Specifying the phi_0, i.e. the expected prediction without any features
+p <- mean(y_train)
+
+# Computing the actual Shapley values with kernelSHAP accounting for feature dependence using
+# the empirical (conditional) distribution approach with bandwidth parameter sigma = 0.1 (default)
+
+explanation <- explain(
+  x_test,
+  approach = "empirical",
+  explainer = explainer,
+  prediction_zero = p
+)
+
+
+print(explanation$dt)
+
+setup <- explain_setup(
+  x_test,
+  approach = "gaussian",
+  explainer = explainer,
+  prediction_zero = p
+)
+
+str(explainer,max.level = 1)
+str(setup,max.level=1)
+
+explainer <- explain_setup(
+  x_test,
+  approach = "empirical",
+  explainer = explainer,
+  prediction_zero = p,
+  n_batches = 4
+)
+
+
+dt <- future.apply::future_lapply(X = explainer$S_batch,
+                                  FUN = batch_prepare_vS,
+                                  explainer = explainer,
+                                  future.seed = explainer$seed)
+dt <- batch_prepare_vS(explainer$S_batch[[4]],explainer)
+
+
+
+
+prepare_data(explainer, index_features = explainer$S_batch[[1]])
+# Printing the Shapley values for the test data.
+# For more information about the interpretation of the values in the table, see ?shapr::explain.
+print(explanation$dt)
+
+# Finally we plot the resulting explanations
+plot(explanation)
