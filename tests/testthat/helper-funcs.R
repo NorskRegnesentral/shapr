3685c2e1cd88d2641bda8f44d62c16d15dd9ff6b
Martin <jullum@nr.no>
2022-05-16 11:57:49 +0200

    cleaning up

---
+++ tests/testthat/helper-funcs.R
@@ -0,0 +1,31 @@
+
+helper_rds <- function(code,filename="tmp.rds"){
+  path <- file.path(tempdir(),filename)
+  saveRDS(code,file = path)
+
+  path
+}
+
+compare_rds <- function(old,new){
+
+  old <- readRDS(old)
+  new <- readRDS(new)
+
+  check <- all.equal(old,new)
+  ifelse(is.character(check),FALSE,check)
+}
+
+snapshot_review_man <- function(path){
+  changed <- testthat:::snapshot_meta(path)
+  these_rds <- (tools::file_ext(changed$name)=="rds")
+  if(any(these_rds)){
+    for(i in which(these_rds)){
+      old <- readRDS(changed[i,"cur"])
+      new <- readRDS(changed[i,"new"])
+
+      cat(paste0("Difference for check ",changed[i,"name"]," in test ", changed[i,"test"],"\n"))
+      print(waldo::compare(old,new))
+      browser()
+    }
+  }
+}
