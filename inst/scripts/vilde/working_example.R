521c3c2700156d6c920b4511787855ab187e5c4d
Vilde Ung <67263002+ungvilde@users.noreply.github.com>
2022-05-30 16:23:11 +0200

    example of explain_final that works

---
+++ inst/scripts/vilde/working_example.R
@@ -0,0 +1,28 @@
+library(xgboost)
+library(shapr)
+library(data.table)
+
+data("Boston", package = "MASS")
+
+x_var <- c("lstat", "rm", "dis", "indus")
+y_var <- "medv"
+
+x_train <- as.matrix(Boston[-1:-6, x_var])
+y_train <- Boston[-1:-6, y_var]
+x_test <- as.matrix(Boston[1:6, x_var])
+
+# Looking at the dependence between the features
+cor(x_train)
+
+# Fitting a basic xgboost model to the training data
+model <- xgboost(
+  data = x_train,
+  label = y_train,
+  nround = 20,
+  verbose = FALSE
+)
+p <- mean(y_train)
+
+# Prepare the data for explanation
+explain_final(x_train,x_test,model,approach="independence",prediction_zero=p,n_batches = 4)
+
