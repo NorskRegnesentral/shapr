75b86248360deed4cf06864164655f3fb5f8ce82
Martin <jullum@nr.no>
2022-06-22 13:41:19 +0200

    moving snapshot_review_man to .Rprofile

--- R/testthat-helpers.R
+++ R/testthat-helpers.R
@@ -32,27 +32,3 @@ expect_snapshot_rds <- function(code,name = "tmp"){
   testthat::expect_snapshot_file(path,compare=compare_rds) # Test the returned object
 }
 
-
-#' Helper function for package development
-#'
-#' This is a manual extension of [testthat::snapshot_review()] which works for the \code{.rds} files used in
-#' this package.
-#'
-#' @param path Character
-#' Gives the relative path to the test files to review
-#'
-#' @export
-snapshot_review_man <- function(path,...){
-  changed <- testthat:::snapshot_meta(path)
-  these_rds <- (tools::file_ext(changed$name)=="rds")
-  if(any(these_rds)){
-    for(i in which(these_rds)){
-      old <- readRDS(changed[i,"cur"])
-      new <- readRDS(changed[i,"new"])
-
-      cat(paste0("Difference for check ",changed[i,"name"]," in test ", changed[i,"test"],"\n"))
-      print(waldo::compare(old,new,...))
-      browser()
-    }
-  }
-}
