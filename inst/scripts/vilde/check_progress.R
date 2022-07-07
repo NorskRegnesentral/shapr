b2f29854297b08a25cc2addc7cc04c2dd55a53d6
Vilde Ung <ungvilde@gmail.com>
2022-07-07 15:22:23 +0200

    small fix

--- inst/scripts/vilde/check_progress.R
+++ inst/scripts/vilde/check_progress.R
@@ -9,9 +9,9 @@ data("Boston", package = "MASS")
 x_var <- c("lstat", "rm", "dis", "indus", "age", "ptratio")
 y_var <- "medv"
 
-x_train <- as.matrix(Boston[-1:-150, x_var])
-y_train <- Boston[-1:-150, y_var]
-x_test <- as.matrix(Boston[1:150, x_var])
+x_train <- as.matrix(Boston[-1:-15, x_var])
+y_train <- Boston[-1:-15, y_var]
+x_test <- as.matrix(Boston[1:15, x_var])
 
 # Fitting a basic xgboost model to the training data
 model <- xgboost(
@@ -27,8 +27,12 @@ plan(multisession, workers=3)
 # when we simply call explain(), no progress bar is shown
 x <- explain(x_train, x_test, model, approach="gaussian", prediction_zero=p, n_batches = 4)
 
+# the handler specifies what kind of progress bar is shown
 # Wrapping explain() in with_progress() gives a progress bar when calling explain()
-x <- with_progress(explain(x_train, x_test, model, approach="empirical", prediction_zero=p, n_batches = 5))
+handlers("txtprogressbar")
+x <- with_progress(
+  explain(x_train, x_test, model, approach="empirical", prediction_zero=p, n_batches = 5)
+  )
 
 # with global=TRUE the progress bar is displayed whenever the explain-function is called, and there is no need to use with_progress()
 handlers(global = TRUE)
