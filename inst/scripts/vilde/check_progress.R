5ec919532469dcf0958e39dc8a1a9fb0ccc6e502
Martin <jullum@nr.no>
2022-07-08 15:23:09 +0200

    Revert "Merge remote-tracking branch 'origin/progress_bar' into progress_bar"
    This reverts commit 2974ec9327f73ffc6faef67b9b2b704559a6b10e.

--- inst/scripts/vilde/check_progress.R
+++ inst/scripts/vilde/check_progress.R
@@ -1,20 +1,17 @@
 library(progressr)
 library(future.apply)
 library(xgboost)
-#library(shapr) # use devtools::load_all() instead when experimenting
+library(shapr)
 library(data.table)
 
-data("airquality")
-airquality <- airquality[complete.cases(airquality), ]
+data("Boston", package = "MASS")
 
-x_var <- c("Solar.R", "Wind", "Temp", "Month")
-y_var <- "Ozone"
+x_var <- c("lstat", "rm", "dis", "indus", "age", "ptratio")
+y_var <- "medv"
 
-ind_x_test <- 1:20
-x_train <- as.matrix(airquality[-ind_x_test, x_var])
-y_train <- airquality[-ind_x_test, y_var]
-x_test <- as.matrix(airquality[ind_x_test, x_var])
-
+x_train <- as.matrix(Boston[-1:-15, x_var])
+y_train <- Boston[-1:-15, y_var]
+x_test <- as.matrix(Boston[1:15, x_var])
 
 # Fitting a basic xgboost model to the training data
 model <- xgboost(
@@ -25,24 +22,32 @@ model <- xgboost(
 )
 p <- mean(y_train)
 
-# By default using rstudio progressbar in RStudio, progress is that is installed, otherwise txtprogressbar
-x <- explain(x_train, x_test, model, approach="gaussian", prediction_zero=p, n_batches = 10,n_samples=10^5)
-#### THIS DOES NOT WORK AS INTENDED -- txtprogressbar is still used when none are specified -- DONT UNDERSTAND WHY
+plan(multisession, workers=3)
 
+# when we simply call explain(), no progress bar is shown
+x <- explain(x_train, x_test, model, approach="gaussian", prediction_zero=p, n_batches = 4)
 
+# the handler specifies what kind of progress bar is shown
+# Wrapping explain() in with_progress() gives a progress bar when calling explain()
+handlers("txtprogressbar")
+x <- with_progress(
+  explain(x_train, x_test, model, approach="empirical", prediction_zero=p, n_batches = 5)
+  )
 
-# Can also set them manually
-progressr::handlers("rstudio")
-x <- explain(x_train, x_test, model, approach="gaussian", prediction_zero=p, n_batches = 10,n_samples=10^5)
+# with global=TRUE the progress bar is displayed whenever the explain-function is called, and there is no need to use with_progress()
+handlers(global = TRUE)
+x <- explain(x_train, x_test, model, approach="gaussian", prediction_zero=p, n_batches = 4)
 
-progressr::handlers("progress")
-x <- explain(x_train, x_test, model, approach="gaussian", prediction_zero=p, n_batches = 10,n_samples=10^5)
+# there are different options for what kind of progress bar should be displayed
+handlers("txtprogressbar") #this is the default
+x <- explain(x_train, x_test, model, approach="independence", prediction_zero=p, n_batches = 4)
+
+handlers("progress")
+x <- explain(x_train, x_test, model, approach="independence", prediction_zero=p, n_batches = 4)
 
 # you can edit the symbol used to draw completed progress in the progress bar (as well as other features) with handler_progress()
 handlers(handler_progress(complete = "#"))
-x <- explain(x_train, x_test, model, approach="gaussian", prediction_zero=p, n_batches = 10,n_samples = 10^5)
+x <- explain(x_train, x_test, model, approach="copula", prediction_zero=p, n_batches = 4)
 
-# This also works for parallelized setups
-plan(multisession,workers = 3)
-x <- explain(x_train, x_test, model, approach="gaussian", prediction_zero=p, n_batches = 10,n_samples=10^5)
+
 
