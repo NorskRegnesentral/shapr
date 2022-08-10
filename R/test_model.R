a45da0f8495435fca75124dc60601715e7b40a3e
Martin <jullum@nr.no>
2022-08-10 09:44:58 +0200

    check_model edit + name change

--- R/test_model.R
+++ R/test_model.R
@@ -3,10 +3,24 @@
 #'
 #' @inheritParams default_doc
 #' @keywords internal
-test_model <- function(internal, model) {
-  x <- head(internal$data$x_train, 2)
-  tmp <- internal$funcs$predict_model(model, x)
-  if (!(all(is.numeric(tmp)) & length(tmp) == 2)) {
+check_model <- function(internal, model) {
+
+  ignore_model <- internal$parameters$ignore_model
+  x_train <- internal$data$x_train
+  predict_model <- internal$funcs$predict_model
+
+  if(ignore_model){
+    stop("`ignore_model=TRUE` is only allowed in the Python wrapper of the shapr package.")
+  }
+
+    if(is.null(model)){
+    stop("Argument `model` is missing.")
+  }
+
+  x <- head(x_train, 2)
+  tmp <- predict_model(model, x)
+  if (!(all(is.numeric(tmp)) &&
+        length(tmp) == 2)) {
     stop(
       paste0(
         "The predict_model function of class ", class(model), " is invalid.\n",
