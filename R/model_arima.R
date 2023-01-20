f18adc697ee2b2499eeeddbe5ac17a5327e4290a
jonlachmann <jon@lachmann.nu>
2023-01-20 17:48:23 +0100

    Add ARIMA model definition.

---
+++ R/model_arima.R
@@ -0,0 +1,50 @@
+#' @rdname predict_model
+#' @export
+predict_model.Arima <- function(x, newdata, newreg, horizon) {
+  if (!requireNamespace("stats", quietly = TRUE)) {
+    stop("The stats package is required for predicting stats models")
+  }
+  
+  n_endo <- max(x$arma[1], x$arma[6] + x$arma[7] * x$arma[5] + 1)
+  prediction <- matrix(NA, nrow(newdata), horizon)
+
+  for (i in seq_len(nrow(newdata))) {
+    endo <- as.numeric(newdata[i, 1:n_endo])
+    exo <- matrix(newdata[i, -(1:n_endo)], n_endo)
+
+    if (ncol(exo) == 0) {
+      x <- forecast::Arima(y = endo, model = x)
+      prediction[i, ] <- predict(x, h = horizon)$pred
+    } else {
+      x <- forecast::Arima(y = endo, xreg = exo, model = x)
+      xreg <- matrix(newreg, n_endo)
+      prediction[i, ] <- predict(x, newxreg=xreg, h = horizon)$pred
+    }
+  }
+
+  as.data.frame(prediction)
+}
+
+#' @rdname get_model_specs
+#' @export
+get_model_specs.Arima <- function(x) {
+  model_checker(x) # Checking if the model is supported
+
+  feature_specs <- list()
+  feature_specs$labels <- rep(NA, length(x$ar))
+  m <- length(feature_specs$labels)
+
+  feature_specs$classes <- rep("numeric", m)
+  names(feature_specs$classes) <- feature_specs$labels
+  feature_specs$factor_levels <- setNames(vector("list", m), feature_specs$labels)
+  feature_specs$factor_levels[names(x$xlevels)] <- NULL
+
+  return(feature_specs)
+}
+
+
+#' @rdname model_checker
+#' @export
+model_checker.Arima <- function(x) {
+  NULL
+}
