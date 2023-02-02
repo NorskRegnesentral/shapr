729d69f7f13f66c0df3a77f62ec8cb61f4bb9ffc
jonlachmann <jon@lachmann.nu>
2023-02-02 17:28:45 +0100

    Update docs. Add failing test to illustrate the need to handle different horizons properly when exogenous variables are present.

--- tests/testthat/test-forecast-output.R
+++ tests/testthat/test-forecast-output.R
@@ -56,3 +56,41 @@ test_that("forecast_output_forecast_ARIMA_group_numeric", {
     "forecast_output_forecast_ARIMA_group_numeric"
   )
 })
+
+
+test_that("ARIMA gives the same output for h = 1 when total horizon = 1 and 2", {
+  set.seed(123)
+  h2 <- explain_forecast(model = model_arima_temp,
+    y = data[1:150, "Temp"],
+    xreg = data[, "Wind"],
+    train_idx = 2:148,
+    explain_idx = 149:150,
+    explain_y_lags = 2,
+    explain_xreg_lags = 2,
+    horizon = 2,
+    approach = "empirical",
+    prediction_zero = p0_ar[1:2],
+    group_lags = FALSE,
+    n_batches = 1,
+    timing = FALSE
+  )
+
+  set.seed(123)
+  h1 <- explain_forecast(model = model_arima_temp,
+    y = data[1:150, "Temp"],
+    xreg = data[, "Wind"],
+    train_idx = 2:148,
+    explain_idx = 149:150,
+    explain_y_lags = 2,
+    explain_xreg_lags = 2,
+    horizon = 1,
+    approach = "empirical",
+    prediction_zero = p0_ar[1],
+    group_lags = FALSE,
+    n_batches = 1,
+    timing = FALSE
+  )
+
+  expect_equivalent(h2$shapley_values[1, -7], h1$shapley_values[1, ])
+
+})
