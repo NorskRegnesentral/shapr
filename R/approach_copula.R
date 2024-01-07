bc3df32874b2fe08ee2b480a0c5319c18a9caff2
LHBO <lholsen@math.uio.no>
2024-01-07 13:48:05 +0100

    Updated approach_copula.R

--- R/approach_copula.R
+++ R/approach_copula.R
@@ -1,8 +1,7 @@
 #' @rdname setup_approach
-#'
 #' @inheritParams default_doc_explain
-#'
 #' @export
+#' @author Martin Jullum
 setup_approach.copula <- function(internal, ...) {
   parameters <- internal$parameters
   x_train <- internal$data$x_train
@@ -31,7 +30,6 @@ setup_approach.copula <- function(internal, ...) {
   )
   parameters$copula.cov_mat <- get_cov_mat(x_train0)
 
-
   x_explain_gaussian <- apply(
     X = rbind(x_explain, x_train),
     MARGIN = 2,
@@ -49,84 +47,104 @@ setup_approach.copula <- function(internal, ...) {
   return(internal)
 }
 
-
-
 #' @inheritParams default_doc
 #' @rdname prepare_data
 #' @export
 #' @author Lars Henry Berge Olsen
-prepare_data.copula2 <- function(internal, index_features, ...) {
-
+prepare_data.copula <- function(internal, index_features, ...) {
   # Extract used variables
-  X <- internal$objects$X
-  x_train <- internal$data$x_train
-  x_train_mat <- as.matrix(internal$data$x_train)
-  x_explain <- internal$data$x_explain
-  x_explain_mat <- as.matrix(internal$data$x_explain)
+  S <- internal$objects$S[index_features, , drop = FALSE]
+  feature_names <- internal$parameters$feature_names
   n_explain <- internal$parameters$n_explain
   n_samples <- internal$parameters$n_samples
-  n_samples = 1000000
   n_features <- internal$parameters$n_features
-  n_combinations <- internal$parameters$n_combinations
   n_combinations_now <- length(index_features)
+  x_train_mat <- as.matrix(internal$data$x_train)
+  x_explain_mat <- as.matrix(internal$data$x_explain)
   copula.mu <- internal$parameters$copula.mu
   copula.cov_mat <- internal$parameters$copula.cov_mat
-  copula.x_explain_gaussian_mat <- as.matrix(internal$data$copula.x_explain_gaussian) # CAN SKIP as.matrix as it is a matrix allready
-  feature_names <- internal$parameters$feature_names
+  copula.x_explain_gaussian_mat <- as.matrix(internal$data$copula.x_explain_gaussian)
 
-
-
-
-
-
-
-
-
-  S <- internal$objects$S[index_features, , drop = FALSE]
-
+  # TODO: Note that `as.matrix` is not needed for `copula.x_explain_gaussian_mat` as it is already defined as a matrix
+  # in `setup_approach.copula`, however, it seems that Martin plans to make it into a data.table, thus, I include
+  # `as.matrix` as future safety. DISCUSS WITH MARTIN WHAT HIS PLANS ARE!
 
   # Generate the MC samples from N(0, 1)
   MC_samples_mat <- matrix(rnorm(n_samples * n_features), nrow = n_samples, ncol = n_features)
 
-  # Use Cpp to convert the MC samples to N(mu_{Sbar|S}, Sigma_{Sbar|S}) for all coalitions and explicands.
-  # The object `dt` is here a 3D array of dimension (n_samples, n_explain*n_coalitions, n_features).
-  # INCLUDE THE TRANSFORMATION
+  # Use C++ to convert the MC samples to N(mu_{Sbar|S}, Sigma_{Sbar|S}), for all coalitions and explicands,
+  # and then transforming them back to the original scale using the inverse Gaussian transform in C++.
+  # The object `dt` is a 3D array of dimension (n_samples, n_explain * n_coalitions, n_features).
+  dt <- prepare_data_copula_cpp(
+    MC_samples_mat = MC_samples_mat,
+    x_explain_mat = x_explain_mat,
+    x_explain_gaussian_mat = copula.x_explain_gaussian_mat,
+    x_train_mat = x_train_mat,
+    S = S,
+    mu = copula.mu,
+    cov_mat = copula.cov_mat
+  )
 
+  # Reshape `dt` to a 2D array of dimension (n_samples * n_explain * n_coalitions, n_features).
+  dim(dt) <- c(n_combinations_now * n_explain * n_samples, n_features)
 
-  dt <- prepare_data_copula_cpp(MC_samples_mat = MC_samples_mat,
-                                  x_explain_mat = x_explain_mat,
-                                  x_explain_gaussian_mat = copula.x_explain_gaussian_mat,
-                                  x_train_mat = x_train_mat,
-                                  S = S,
-                                  mu = copula.mu,
-                                  cov_mat = copula.cov_mat)
-
-  # Reshape `dt` to a 2D array of dimension (n_samples*n_explain*n_coalitions, n_features).
-  dim(dt) = c(n_combinations_now*n_explain*n_samples, n_features)
-
-  # Convert to a data.table
-  dt = as.data.table(dt)
-  setnames(dt, feature_names)
-  dt[, "id_combination" := rep(seq(nrow(S)), each = n_samples * n_explain)]
-  dt[, "id" := rep(seq(n_explain), each = n_samples, times = nrow(S))]
-  dt[, "w" := 1 / n_samples]
-  data.table::setcolorder(dt, c("id_combination", "id", feature_names))
+  # Convert to a data.table and add extra identification columns
+  dt <- data.table::as.data.table(dt)
+  data.table::setnames(dt, feature_names)
+  dt[, id_combination := rep(seq(nrow(S)), each = n_samples * n_explain)]
+  dt[, id := rep(seq(n_explain), each = n_samples, times = nrow(S))]
+  dt[, w := 1 / n_samples]
   dt[, id_combination := index_features[id_combination]]
-  dt
-
-  dt[id_combination == 9 & id == 1,]
-
-  dt_agr = dt[, lapply(.SD, mean), by = list(id, id_combination)]
-  data.table::setorderv(dt_agr, c("id", "id_combination"))
-  dt_agr
+  data.table::setcolorder(dt, c("id_combination", "id", feature_names))
 
   return(dt)
 }
 
+#' Transforms new data to standardized normal (dimension 1) based on other data transformations
+#'
+#' @param yx Numeric vector. The first `n_y` items is the data that is transformed, and last
+#' part is the data with the original transformation.
+#' @param n_y Positive integer. Number of elements of `yx` that belongs to the Gaussian data.
+#'
+#' @return Vector of back-transformed Gaussian data
+#'
+#' @keywords internal
+#' @author Martin Jullum
+gaussian_transform_separate <- function(yx, n_y) {
+  if (n_y >= length(yx)) stop("n_y should be less than length(yx)")
+  ind <- 1:n_y
+  x <- yx[-ind]
+  tmp <- rank(yx)[ind]
+  tmp <- tmp - rank(tmp) + 0.5
+  u_y <- tmp / (length(x) + 1)
+  z_y <- stats::qnorm(u_y)
+  return(z_y)
+}
+
+#' Transforms a sample to standardized normal distribution
+#'
+#' @param x Numeric vector.The data which should be transformed to a standard normal distribution.
+#'
+#' @return Numeric vector of length `length(x)`
+#'
+#' @keywords internal
+#' @author Martin Jullum
+gaussian_transform <- function(x) {
+  u <- rank(x) / (length(x) + 1)
+  z <- stats::qnorm(u)
+  return(z)
+}
+
+
+
+# TRASH BELOW -----------------------------------------------------------------------------------------------------
+
+
+
 #' @inheritParams default_doc
 #' @rdname prepare_data
 #' @export
-prepare_data.copula <- function(internal, index_features = NULL, ...) {
+prepare_data.copula_old <- function(internal, index_features = NULL, ...) {
   X <- internal$objects$X
   x_train <- internal$data$x_train
   x_explain <- internal$data$x_explain
@@ -147,39 +165,37 @@ prepare_data.copula <- function(internal, index_features = NULL, ...) {
   }
 
 
-  n_samples = 1000000
-  for (i in seq_len(n_explain)) {
-    print(i)
-    l <- lapply(
-      X = features,
-      FUN = sample_copula,
-      n_samples = n_samples,
-      mu = copula.mu,
-      cov_mat = copula.cov_mat,
-      m = n_features,
-      x_explain = x_explain0[i, , drop = FALSE],
-      x_train = as.matrix(x_train),
-      x_explain_gaussian = copula.x_explain_gaussian[i, , drop = FALSE]
-    )
-    # x_explain_gaussian2 = x_explain_gaussian
-    # x_train2 = x_train
-    # x_explain2 = x_explain
+  n_samples <- 5000000
+  system.time({
+    for (i in seq_len(n_explain)) {
+      print(i)
+      l <- lapply(
+        X = features,
+        FUN = sample_copula,
+        n_samples = n_samples,
+        mu = copula.mu,
+        cov_mat = copula.cov_mat,
+        m = n_features,
+        x_explain = x_explain0[i, , drop = FALSE],
+        x_train = as.matrix(x_train),
+        x_explain_gaussian = copula.x_explain_gaussian[i, , drop = FALSE]
+      )
+      # x_explain_gaussian2 = x_explain_gaussian
+      # x_train2 = x_train
+      # x_explain2 = x_explain
 
-    dt_l[[i]] <- data.table::rbindlist(l, idcol = "id_combination")
-    dt_l[[i]][, w := 1 / n_samples]
-    dt_l[[i]][, id := i]
-    if (!is.null(index_features)) dt_l[[i]][, id_combination := index_features[id_combination]]
-  }
+      dt_l[[i]] <- data.table::rbindlist(l, idcol = "id_combination")
+      dt_l[[i]][, w := 1 / n_samples]
+      dt_l[[i]][, id := i]
+      if (!is.null(index_features)) dt_l[[i]][, id_combination := index_features[id_combination]]
+    }
+  })
   dt <- data.table::rbindlist(dt_l, use.names = TRUE, fill = TRUE)
   dt
-  dt9 = dt
-  dt9_agr = dt9[, lapply(.SD, mean), by = list(id, id_combination)]
-  dt9_agr - dt_agr
+  dt9 <- dt
+  dt9_agr <- dt9[, lapply(.SD, mean), by = list(id, id_combination)]
+  (dt9_agr - dt_agr) / dt9_agr
   return(dt)
-
-
-
-
 }
 
 #' Sample conditional variables using the Gaussian copula approach
@@ -225,10 +241,10 @@ sample_copula <- function(index_given, n_samples, mu, cov_mat, m, x_explain_gaus
       n_z = n_samples,
       type = 5
     )
-    ret0_x_cpp = inv_gaussian_transform_cpp(z = ret0_z, x = x_train[, dependent_ind, drop = FALSE])
+    ret0_x_cpp <- inv_gaussian_transform_cpp(z = ret0_z, x = x_train[, dependent_ind, drop = FALSE])
 
-    ret0_x_cpp = inv_gaussian_transform_cpp_armamat(rbind(ret0_z, x_train[, dependent_ind, drop = FALSE]), n_samples = n_samples)
-    colnames(ret0_x_cpp) = feature_names[dependent_ind]
+    ret0_x_cpp <- inv_gaussian_transform_cpp_armamat(rbind(ret0_z, x_train[, dependent_ind, drop = FALSE]), n_samples = n_samples)
+    colnames(ret0_x_cpp) <- feature_names[dependent_ind]
     all.equal(ret0_x, ret0_x_cpp)
 
     # Dette har jeg kode til
@@ -262,39 +278,4 @@ inv_gaussian_transform <- function(zx, n_z, type) {
   return(as.double(x_new))
 }
 
-#' Transforms new data to standardized normal (dimension 1) based on other data transformations
-#'
-#' @param yx Numeric vector. The first `n_y` items is the data that is transformed, and last
-#' part is the data with the original transformation.
-#' @param n_y Positive integer. Number of elements of `yx` that belongs to the gaussian data.
-#'
-#' @return Vector of back-transformed Gaussian data
-#'
-#' @keywords internal
-#'
-#' @author Martin Jullum
-gaussian_transform_separate <- function(yx, n_y) {
-  if (n_y >= length(yx)) stop("n_y should be less than length(yx)")
-  ind <- 1:n_y
-  x <- yx[-ind]
-  tmp <- rank(yx)[ind]
-  tmp <- tmp - rank(tmp) + 0.5
-  u_y <- tmp / (length(x) + 1)
-  z_y <- stats::qnorm(u_y)
-  return(z_y)
-}
 
-#' Transforms a sample to standardized normal distribution
-#'
-#' @param x Numeric vector.The data which should be transformed to a standard normal distribution.
-#'
-#' @return Numeric vector of length `length(x)`
-#'
-#' @keywords internal
-#'
-#' @author Martin Jullum
-gaussian_transform <- function(x) {
-  u <- rank(x) / (length(x) + 1)
-  z <- stats::qnorm(u)
-  return(z)
-}
