e7e8229d10cf11f658bd740e62966daee4d4f8dc
Martin <jullum@nr.no>
2024-01-15 15:23:07 +0100

    script for testing pure permuting

--- inst/scripts/devel/devel_permute.R
+++ inst/scripts/devel/devel_permute.R
@@ -40,5 +40,45 @@ test_permute <-explain(model = model_lm_numeric,
 test_kernel$shapley_values-test_permute$shapley_values
 
 
+permute_list <- kernel_list <- list()
+for(i in 1:10){
+  set.seed(1+i)
 
-debugonce(explain)
+  test_kernel <-explain(model = model_lm_numeric,
+                        x_explain = x_explain_numeric,
+                        x_train = x_train_numeric,
+                        approach = "empirical",
+                        shap_approach = "kernel",
+                        prediction_zero = p0,n_combinations = 2^5-3,seed = i)
+
+
+  test_permute <-explain(model = model_lm_numeric,
+                         x_explain = x_explain_numeric,
+                         x_train = x_train_numeric,
+                         approach = "empirical",
+                         shap_approach = "permutation",
+                         prediction_zero = p0,n_permutations = factorial(5)-3,seed=i)
+  permute_list[[i]] <- test_permute$shapley_values
+  kernel_list[[i]] <- test_kernel$shapley_values
+
+  print(i)
+
+}
+
+permute_list <- lapply(permute_list,as.matrix)
+kernel_list <- lapply(kernel_list,as.matrix)
+
+# permute is way more precise than kernel when sampling
+
+Reduce(`+`,permute_list)/length(permute_list)
+
+Reduce(`+`,kernel_list)/length(kernel_list)
+
+explain(model = model_lm_numeric,
+        x_explain = x_explain_numeric,
+        x_train = x_train_numeric,
+        approach = "empirical",
+        shap_approach = "kernel",
+        prediction_zero = p0)
+
+
