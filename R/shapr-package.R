93311faa13977a88283e35f3b263a6e634bf831c
LHBO <lholsen@math.uio.no>
2024-02-16 14:27:37 +0000

    Updated questions before publising on cran

--- R/shapr-package.R
+++ R/shapr-package.R
@@ -35,10 +35,11 @@ NULL
 #' @keywords internal
 "_PACKAGE"
 
-
 #' Auxiliary function for the vaeac vignette
-#' @description Function that question if the vaeac vignette has been built using `rebuild-long-running-vignette.R`.
-#' This is only useful when using devtools to release `shapr` to cran. See [devtools::release()] for more information.
+#' @description Function that question if the main and vaeac vignette has been built using the
+#' `rebuild-long-running-vignette.R` function. This is only useful when using devtools to release
+#' `shapr` to cran. See [devtools::release()] for more information.
 release_questions <- function() {
-  c("Did you re-build the `understanding_shapr_vaeac.Rmd` using `rebuild-long-running-vignette.R`?")
+  c(paste0("Did you re-build the `understanding_shapr.Rmd` and `understanding_shapr_vaeac.Rmd` vignettes using ",
+           "`rebuild-long-running-vignette.R`?"))
 }
