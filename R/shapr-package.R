034dea26338337f913b177e43ec0ec320a61a553
LHBO <lholsen@math.uio.no>
2024-02-19 10:11:01 +0000

    Figuring out what imports to remove

--- R/shapr-package.R
+++ R/shapr-package.R
@@ -1,13 +1,12 @@
-#' @importFrom data.table data.table fread fwrite setnames := year month
-#' uniqueN setkey as.data.table copy between is.data.table setcolorder rbindlist
+#' #importFrom data.table data.table fread fwrite setnames := year month uniqueN setkey as.data.table copy between is.data.table setcolorder rbindlist
 #'
-#' @importFrom graphics plot hist
+#' #importFrom graphics plot hist
 #'
-#' @importFrom utils head tail methods modifyList
+#' #importFrom utils head tail methods modifyList
 #'
-#' @importFrom stats predict as.formula formula model.matrix setNames embed sd qt pt rnorm
+#' @importFrom stats predict as.formula formula setNames embed sd qt pt rnorm
 #'
-#' @importFrom Rcpp sourceCpp
+#' #importFrom Rcpp sourceCpp
 #'
 #' @keywords internal
 #'
