f7cc967cfab26084aee17c4f3e4eff25badfd224
Martin <jullum@nr.no>
2024-02-21 12:18:31 +0100

    remove purl call

--- rebuild_long_running_vignette.R
+++ rebuild_long_running_vignette.R
@@ -6,9 +6,9 @@ old_wd <- getwd()
 
 setwd("vignettes/")
 knitr::knit("understanding_shapr_vaeac.Rmd.orig", output = "understanding_shapr_vaeac.Rmd")
-knitr::purl("understanding_shapr_vaeac.Rmd.orig", output = "understanding_shapr_vaeac.R")
+#knitr::purl("understanding_shapr_vaeac.Rmd.orig", output = "understanding_shapr_vaeac.R") # Don't need this
 
 knitr::knit("understanding_shapr.Rmd.orig", output = "understanding_shapr.Rmd")
-knitr::purl("understanding_shapr.Rmd.orig", output = "understanding_shapr.R")
+#knitr::purl("understanding_shapr.Rmd.orig", output = "understanding_shapr.R") # Don't need this
 
 setwd(old_wd)
