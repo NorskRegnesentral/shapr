89938457a22db4b79a59baa9896ef0bcd134a7b0
LHBO <lholsen@math.uio.no>
2024-02-28 12:25:35 +0000

    Refactored surrogate

--- R/approach_regression_surrogate.R
+++ R/approach_regression_surrogate.R
@@ -1,16 +1,15 @@
 # Shapr functions ======================================================================================================
 #' @rdname setup_approach
 #'
-#' @param regression_model A tidymodels object of class `model_specs`. Default is a linear regression model
-#' ([parsnip::linear_reg()]). See \href{https://www.tidymodels.org/find/parsnip/}{tidymodels} for all possible models.
-#' @param regression_tune_values Either NULL (default) or a data.frame/data.table/tibble containing the possible
-#' hyper-parameter value combinations to try. The column names must match the names of the tuneable parameters specified
-#' in `regression_model`.
-#' @param regression_vfold_cv_para Either NULL (default) or a list containing
-#' the parameters to be sent to [rsample::vfold_cv()].
-#' @param regression_recipe_func Either NULL (default) or a function that must return
-#' the RHS of the formula for arbitrary feature name inputs.
 #' @inheritParams default_doc_explain
+#' @inheritParams setup_approach.regression_separate
+#' @param regression_surr_n_comb Integer (default is `internal$parameters$used_n_combinations`) specifying the number
+#' of unique combinations/coalitions to apply to each training observation. Maximum allowed value is
+#' "`internal$parameters$used_n_combinations` - 2". By default, we use all coalitions, but this can take a lot of memory
+#' in larger dimensions. Note that by "all", we mean all coalitions chosen by `shapr` to be used. This will be all
+#' \eqn{2^{n_{\text{features}}}} coalitions (minus empty and grand coalition) if `shapr` is in the exact mode. If the
+#' user sets a lower value than `internal$parameters$used_n_combinations`, then we sample this amount of unique
+#' coalitions separately for each training observations. That is, on average, all coalitions should be equally trained.
 #'
 #' @export
 #' @author Lars Henry Berge Olsen
@@ -19,6 +18,7 @@ setup_approach.regression_surrogate <- function(internal,
                                                 regression_tune_values = NULL,
                                                 regression_vfold_cv_para = NULL,
                                                 regression_recipe_func = NULL,
+                                                regression_surr_n_comb = internal$parameters$used_n_combinations - 2,
                                                 ...) {
   # Check that tidymodels is installed
   if (!requireNamespace("tidymodels", quietly = TRUE)) {
@@ -26,88 +26,36 @@ setup_approach.regression_surrogate <- function(internal,
   }
 
   # Small printout to the user
-  if (internal$parameters$verbose == 2) {
-    message("Starting 'setup_approach.regression_surrogate'.")
-  }
-
-  # Check that the model outputs one-dimensional predictions
-  if (internal$parameters$output_size != 1) {
-    stop("`regression_separate` only supports models with one-dimensional output")
-  }
-
-  # Check that we are not to keep the Monte Carlo samples
-  if (internal$parameters$keep_samp_for_vS) {
-    stop("`keep_samp_for_vS` must be `FALSE` as there are no Monte Carlo samples to keep for this approach.")
-  }
+  if (internal$parameters$verbose == 2) message("Starting 'setup_approach.regression_surrogate'.")
 
   # Add the default parameter values for the non-user specified parameters for the separate regression approach
-  defaults <-
-    mget(c("regression_model", "regression_tune_values", "regression_vfold_cv_para", "regression_recipe_func"))
+  defaults <- mget(c(
+    "regression_model", "regression_tune_values", "regression_vfold_cv_para",
+    "regression_recipe_func", "regression_surr_n_comb"
+  ))
   internal <- insert_defaults(internal, defaults)
 
-  # Check that it is a function that returns the RHS of the formula for arbitrary feature name inputs
-  check_regression_recipe_func(internal$parameters$regression_recipe_func)
+  # Check the parameters to the regression approach
+  internal <- check_regression_parameters(internal)
 
-  # Check that `regression_vfold_cv_para` is either NULL or a named list that only contains recognized parameters
-  check_regression_vfold_cv_para(internal$parameters$regression_vfold_cv_para)
-
-  # Check and get if we are to tune the hyperparameters of the regression model
-  internal$parameters$regression_tune <-
-    get_regression_tune(internal$parameters$regression_model, internal$parameters$regression_tune_values)
-
-
-  # TODO: comb_prob = if (use_Shapley_weights) internal$objects$X$shapley_weight[2:n_comb_max] else NULL
-
-  # Predict the response of the training and explain data. Former is the response the regression models are fitted to
-  model <- eval.parent(match.call()[["model"]])
-  internal$data$x_train_y_hat <- predict_model(model, internal$data$x_train)
-  internal$data$x_explain_y_hat <- predict_model(model, internal$data$x_explain)
+  # Get the predicted response of the training and explain data
+  internal <- get_regression_y_hat(internal = internal, model = eval.parent(match.call()[["model"]]))
 
   # Augment the training data
-  x_train_augmented <- regression_surr_augment(internal, x = internal$data$x_train, y_hat = internal$data$x_train_y_hat)
+  x_train_augmented <-
+    regression_surrogate_augment(internal, x = internal$data$x_train, y_hat = internal$data$x_train_y_hat)
 
-  # Create a recipe to the augmented training data
-  regression_recipe <- recipes::recipe(y_hat ~ ., data = x_train_augmented)
-
-  # Update the recipe if user has provided a function for this. User is responsible for that the function works.
-  # This function can, e.g., add transformations, normalization, dummy encoding, interactions, and so on.
-  if (!is.null(regression_recipe_func)) regression_recipe <- regression_recipe_func(regression_recipe)
-
-  # Combine workflow, model specification, and recipe
-  regression_workflow <-
-    workflows::workflow() %>%
-    workflows::add_model(regression_model) %>%
-    workflows::add_recipe(regression_recipe)
-
-  # Check if we are to tune hyperparameters in the regression model, as we then need to update the workflow.
-  # If we are not doing any hyperparameter tuning, then the workflow above is enough.
-  if (regression_tune) {
-    if (verbose == 2) message("Start tuning model...")
-
-    # Set up the V-fold cross validation using the user provided parameters in `regression_vfold_cv_para`.
-    # Note if `regression_vfold_cv_para` is NULL, then we use the default parameters in `vfold_cv()`.
-    regression_folds <- do.call(rsample::vfold_cv, c(list(data = x_train_augmented), regression_vfold_cv_para))
-
-    # Add the hyperparameter tuning to the workflow
-    regression_results <-
-      regression_workflow %>%
-      tune::tune_grid(
-        resamples = regression_folds,
-        grid = regression_tune_values,
-        metrics = yardstick::metric_set(rmse)
-      )
-
-    # Small printout to the user
-    if (verbose == 2) print(regression_results %>% tune::collect_metrics(), n = 10^4) # Large number to print all rows
-
-    # Update the workflow by finalizing it using the hyperparameters that attained the best rmse
-    regression_workflow <-
-      regression_workflow %>%
-      tune::finalize_workflow(regression_results %>% tune::select_best("rmse"))
-  }
-
-  regression_fit <- regression_workflow %>% fit(data = x_train_augmented) # Fit the model to the augmented training data
-  internal$objects$regression_surrogate_model <- regression_fit # Store the fitted model in the internal list
+  # Fit the surrogate regression model and store it in the internal list
+  if (internal$parameters$verbose == 2) message("Start training the surrogate model...")
+  internal$objects$regression_surrogate_model <- regression_train(
+    x = x_train_augmented,
+    regression_model = internal$parameters$regression_model,
+    regression_tune = internal$parameters$regression_tune,
+    regression_tune_values = internal$parameters$regression_tune_values,
+    regression_vfold_cv_para = internal$parameters$regression_vfold_cv_para,
+    regression_recipe_func = internal$parameters$regression_recipe_func,
+    verbose = internal$parameters$verbose
+  )
 
   # Small printout to the user
   if (internal$parameters$verbose == 2) message("Done with 'setup_approach.regression_surrogate'.\n")
@@ -121,8 +69,12 @@ setup_approach.regression_surrogate <- function(internal,
 #' @export
 #' @author Lars Henry Berge Olsen
 prepare_data.regression_surrogate <- function(internal, index_features = NULL, ...) {
+  # Small printout to the user about which batch that are currently worked on
+  if (internal$parameters$verbose == 2) regression_prep_data_message(internal = internal)
+
   # Augment the explicand data
-  x_explain_augmented <- regression_surr_augment(internal, x = internal$data$x_explain, index_features = index_features)
+  x_explain_augmented <-
+    regression_surrogate_augment(internal, x = internal$data$x_explain, index_features = index_features)
 
   # Compute the predicted response for the explicands, i.e., v(S, x_i) for all explicands x_i and S in index_features
   pred_explicand <- predict(internal$objects$regression_surrogate_model, new_data = x_explain_augmented)$.pred
@@ -140,8 +92,7 @@ prepare_data.regression_surrogate <- function(internal, index_features = NULL, .
 #' Augment the training data and the explicands
 #'
 #' @inheritParams default_doc
-#' @param x Data.table containing the data. Either the training data or the explicands. If `x` is the explicands,
-#' then `index_features` must be provided.
+#' @inheritParams regression_train
 #' @param y_hat Vector of numerics (optional) containing the predicted responses for the observations in `x`.
 #' @param index_features Array of integers (optional) containing which coalitions to consider. Must be provided if
 #' `x` is the explicands.
@@ -153,7 +104,8 @@ prepare_data.regression_surrogate <- function(internal, index_features = NULL, .
 #' to factors. If `FALSE`, then the binary masks are numerics.
 #' @param augment_comb_prob Array of numerics (default is `NULL`). The length of the array must match the number of
 #' combinations being considered, where each entry specifies the probability of sampling the corresponding coalition.
-#' This is useful if we want to generate more training data for some specific coalitions.
+#' This is useful if we want to generate more training data for some specific coalitions. One possible choice would be
+#' `augment_comb_prob = if (use_Shapley_weights) internal$objects$X$shapley_weight[2:actual_n_combinations] else NULL`.
 #' @param augment_weights String (optional). Specifying which type of weights to add to the observations.
 #' If `NULL` (default), then no weights are added. If `"Shapley"`, then the Shapley weights for the different
 #' combinations are added to corresponding observations where the coalitions was applied. If `uniform`, then
@@ -162,30 +114,28 @@ prepare_data.regression_surrogate <- function(internal, index_features = NULL, .
 #' @return A data.table containing the augmented data.
 #' @author Lars Henry Berge Olsen
 #' @keywords internal
-regression_surr_augment <- function(internal,
-                                    x,
-                                    y_hat = NULL,
-                                    index_features = NULL,
-                                    augment_masks_as_factor = FALSE,
-                                    augment_include_grand = FALSE,
-                                    augment_add_id_comb = FALSE,
-                                    augment_comb_prob = NULL,
-                                    augment_weights = NULL) {
-  if (!is.null(augment_weights)) augment_weights <- match.arg(augment_weights, c("Shapley", "uniform"))
-
+regression_surrogate_augment <- function(internal,
+                                         x,
+                                         y_hat = NULL,
+                                         index_features = NULL,
+                                         augment_masks_as_factor = FALSE,
+                                         augment_include_grand = FALSE,
+                                         augment_add_id_comb = FALSE,
+                                         augment_comb_prob = NULL,
+                                         augment_weights = NULL) {
   # Get some of the parameters
   S <- internal$objects$S
-  n_comb_max <- internal$parameters$used_n_combinations - 2 # Remove empty and grand coalitions
-  if (augment_include_grand) n_comb_max <- n_comb_max + 1 # Update based on if we are to include the grand comb
-  n_comb_per_observation <- internal$parameters$n_comb_per_observation
-  if (!is.null(index_features)) n_comb_per_observation <- length(index_features)
-
-  # TODO: remove when added check before
-  if (n_comb_per_observation > n_comb_max) n_comb_per_observation <- n_comb_max
+  actual_n_combinations <- internal$parameters$used_n_combinations - 2 # Remove empty and grand coalitions
+  if (augment_include_grand) actual_n_combinations <- actual_n_combinations + 1 # Add 1 to include the grand comb
+  regression_surr_n_comb <- internal$parameters$regression_surr_n_comb
+  if (!is.null(index_features)) regression_surr_n_comb <- length(index_features)
+  if (regression_surr_n_comb > actual_n_combinations) regression_surr_n_comb <- actual_n_combinations
 
   # Small checks
-  if (!is.null(augment_comb_prob) && length(augment_comb_prob) != n_comb_max) {
-    stop(paste("`augment_comb_prob` must be of length", n_comb_max, "."))
+  if (!is.null(augment_weights)) augment_weights <- match.arg(augment_weights, c("Shapley", "uniform"))
+
+  if (!is.null(augment_comb_prob) && length(augment_comb_prob) != actual_n_combinations) {
+    stop(paste("`augment_comb_prob` must be of length", actual_n_combinations, "."))
   }
 
   if (augment_include_grand && augment_shapley_weights) {
@@ -209,13 +159,13 @@ regression_surr_augment <- function(internal,
 
   # Check if we are to augment the training data or the explicands
   if (is.null(index_features)) {
-    # Training: get matrix of dimension n_obs x n_comb_per_observation containing the indices of the active coalitions
-    if (n_comb_per_observation >= n_comb_max) { # Start from two to exclude the empty set
-      # TODO: remove `comb_active_idx <- matrix(rep(seq(2, n_comb_max), times = n_obs), nrow = n_obs, byrow = TRUE)`
-      comb_active_idx <- matrix(rep(seq(2, n_comb_max + 1), times = n_obs), ncol = n_obs)
+    # Training: get matrix of dimension n_obs x regression_surr_n_comb containing the indices of the active coalitions
+    if (regression_surr_n_comb >= actual_n_combinations) { # Start from two to exclude the empty set
+      # TODO: remove `comb_active_idx <- matrix(rep(seq(2, actual_n_combinations), times = n_obs), nrow = n_obs, byrow = TRUE)`
+      comb_active_idx <- matrix(rep(seq(2, actual_n_combinations + 1), times = n_obs), ncol = n_obs)
     } else {
       comb_active_idx <- sapply(seq(n_obs), function(x) { # Add 1 as we want to exclude the empty set
-        sample.int(n = n_comb_max, size = n_comb_per_observation, prob = augment_comb_prob) + 1
+        sample.int(n = actual_n_combinations, size = regression_surr_n_comb, prob = augment_comb_prob) + 1
       })
     }
   } else {
@@ -230,7 +180,7 @@ regression_surr_augment <- function(internal,
   colnames(comb_active) <- names(feature_classes)
 
   # Repeat the feature values as many times as there are active coalitions
-  x_augmented <- x[rep(seq_len(n_obs), each = n_comb_per_observation), ]
+  x_augmented <- x[rep(seq_len(n_obs), each = regression_surr_n_comb), ]
 
   # Mask the categorical features. Add a new level called "level_masked" when value is masked.
   x_augmented[, (feature_cat) := lapply(seq_along(.SD), function(col) {
@@ -241,8 +191,8 @@ regression_surr_augment <- function(internal,
 
   # Mask the continuous/non-categorical features
   x_augmented[, (feature_cont) :=
-                lapply(seq_along(.SD), function(col) .SD[[col]] * comb_active[, feature_cont_idx[col]]),
-              .SDcols = feature_cont
+    lapply(seq_along(.SD), function(col) .SD[[col]] * comb_active[, feature_cont_idx[col]]),
+  .SDcols = feature_cont
   ]
 
   # Add new columns indicating when the continuous features are masked
@@ -261,7 +211,28 @@ regression_surr_augment <- function(internal,
   if (augment_add_id_comb) x_augmented[, ("id_comb") := factor(id_comb)]
 
   # Add repeated responses if provided
-  if (!is.null(y_hat)) x_augmented[, ("y_hat") := rep(y_hat, each = n_comb_per_observation)]
+  if (!is.null(y_hat)) x_augmented[, ("y_hat") := rep(y_hat, each = regression_surr_n_comb)]
 
+  # Return the augmented data
   return(x_augmented)
+}
+
+
+# Check function =======================================================================================================
+#' Check the `regression_surr_n_comb` parameter
+#'
+#' Check that `regression_surr_n_comb` is either NULL or a valid integer.
+#'
+#' @inheritParams setup_approach.regression_surrogate
+#' @param used_n_combinations Integer. The number of used combinations (including the empty and grand coalitions).
+#'
+#' @author Lars Henry Berge Olsen
+#' @keywords internal
+check_regression_n_comb <- function(regression_surr_n_comb, used_n_combinations) {
+  if (!is.null(regression_surr_n_comb) && regression_surr_n_comb > used_n_combinations - 2) {
+    stop(paste0(
+      "`regression_surr_n_comb` (", regression_surr_n_comb, ") must be less than or equal to ",
+      "`used_n_combinations` minus two (", used_n_combinations - 2, ")."
+    ))
+  }
 }
