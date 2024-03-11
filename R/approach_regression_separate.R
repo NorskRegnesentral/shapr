781c025c9975f0bf24cd11f8847a694f087aed77
LHBO <lholsen@math.uio.no>
2024-03-11 12:11:21 +0000

    Added option for `regression_tune_values` to be a function. Needed for, e.g., ranger and mtry, where mtry cannot be larger than n_features in each combination. The function can then take this into consideration and change the hyperparameter values for each coalition size

--- R/approach_regression_separate.R
+++ R/approach_regression_separate.R
@@ -1,14 +1,18 @@
 # Shapr functions ======================================================================================================
 #' @rdname setup_approach
 #'
-#' @param regression_model A tidymodels object of class `model_specs`. Default is a linear regression model
-#' ([parsnip::linear_reg()]). See \href{https://www.tidymodels.org/find/parsnip/}{tidymodels} for all possible models.
-#' @param regression_tune_values Either NULL (default) or a data.frame/data.table/tibble containing the possible
-#' hyper-parameter value combinations to try. The column names must match the names of the tuneable parameters specified
-#' in `regression_model`.
-#' @param regression_vfold_cv_para Either NULL (default) or a list containing
+#' @param regression_model A `tidymodels` object of class `model_specs`. Default is a linear regression model
+#' ([parsnip::linear_reg()]). See \href{https://www.tidymodels.org/find/parsnip/}{tidymodels} for all possible models,
+#' and see the vignette for how to add new/own models.
+#' @param regression_tune_values Either `NULL` (default), a data.frame/data.table/tibble, or a function.
+#' The data.frame must contain the possible hyperparameter value combinations to try.
+#' The column names must match the names of the tuneable parameters specified in `regression_model`.
+#' If `regression_tune_values` is a function, then it should take one argument `x` which is the training data
+#' for the current combination/coalition and returns a data.frame/data.table/tibble with the properties described above.
+#' Using a function allows the hyperparameter values to change based on the size of the combination.
+#' @param regression_vfold_cv_para Either `NULL` (default) or a list containing
 #' the parameters to be sent to [rsample::vfold_cv()].
-#' @param regression_recipe_func Either NULL (default) or a function that must return
+#' @param regression_recipe_func Either `NULL` (default) or a function that must return
 #' the RHS of the formula for arbitrary feature name inputs.
 #' @inheritParams default_doc_explain
 #'
@@ -35,7 +39,7 @@ setup_approach.regression_separate <- function(internal,
   internal <- insert_defaults(internal, defaults)
 
   # Check the parameters to the regression approach
-  internal <- check_regression_parameters(internal)
+  internal <- check_regression_parameters(internal = internal)
 
   # Get the predicted response of the training and explain data
   internal <- get_regression_y_hat(internal = internal, model = eval.parent(match.call()[["model"]]))
@@ -171,16 +175,23 @@ regression_train <- function(x,
       }
     }
 
+    # Extract the grid of hyperparameter values. Note that regression_tune_values is either a data.frame or a function.
+    if (is.data.frame(regression_tune_values)) {
+      regression_grid <- regression_tune_values
+    } else {
+      regression_grid <- regression_tune_values(x[, -..regression_response_var])
+    }
+
     # Add the hyperparameter tuning to the workflow
     regression_results <- tune::tune_grid(
       object = regression_workflow,
       resamples = regression_folds,
-      grid = regression_tune_values,
+      grid = regression_grid,
       metrics = yardstick::metric_set(yardstick::rmse)
     )
 
     # Small printout to the user
-    if (verbose == 2) regression_cv_message(regression_results, regression_tune_values)
+    if (verbose == 2) regression_cv_message(regression_results = regression_results, regression_grid = regression_grid)
 
     # Update the workflow by finalizing it using the hyperparameters that attained the best rmse
     regression_workflow <- tune::finalize_workflow(regression_workflow, tune::select_best(regression_results, "rmse"))
@@ -212,20 +223,21 @@ get_regression_y_hat <- function(internal, model) {
 
 #' Get if model is to be tuned
 #'
-#' That is, if the regression model contains hyper-parameters we are to tune using cross validation.
-#' See \href{https://www.tidymodels.org/find/parsnip/#model-args}{tidymodels} for default model hyper-parameters.
+#' That is, if the regression model contains hyperparameters we are to tune using cross validation.
+#' See \href{https://www.tidymodels.org/find/parsnip/#model-args}{tidymodels} for default model hyperparameters.
 #'
 #' @inheritParams setup_approach.regression_separate
+#' @inheritParams explain
 #'
 #' @author Lars Henry Berge Olsen
 #' @keywords internal
-get_regression_tune <- function(regression_model, regression_tune_values) {
+get_regression_tune <- function(regression_model, regression_tune_values, x_train) {
   # Check that the regression model is a tidymodels object
   if (is.null(regression_model) || !"model_spec" %in% class(regression_model)) {
     stop("`regression_model` must be a tidymodels object with class 'model_spec'. See documentation.")
   }
 
-  # Check if we are to tune some model hyper-parameters
+  # Check if we are to tune some model hyperparameters
   regression_para <- lapply(regression_model$args, function(para) rlang::quo_get_expr(para))
   regression_para_tune <- lapply(regression_para, function(para) !is.null(para) && para == "tune()")
   regression_para_tune_names <- names(regression_para_tune)[unlist(regression_para_tune)]
@@ -233,13 +245,29 @@ get_regression_tune <- function(regression_model, regression_tune_values) {
 
   # Check that user have provided a tuning
   if (isTRUE(regression_tune) && is.null(regression_tune_values)) {
-    stop("`regression_tune_values` must be provided when `regression_model` contains hyper-parameters to tune.")
+    stop("`regression_tune_values` must be provided when `regression_model` contains hyperparameters to tune.")
   }
 
-  # Get the names of the hyper-parameters the user provided values for
-  regression_tune_values_names <- names(regression_tune_values)
+  # Check function or tibble
+  if (!is.null(regression_tune_values) &&
+      !is.data.frame(regression_tune_values) &&
+      !is.function(regression_tune_values)) {
+    stop("`regression_tune_values` must be of either class `data.frame` or `function`. See documentation.")
+  }
 
-  # Check that user have provided values for the hyper-parameters to tune
+  # Get the grid values. And if user provided a function, then check that it is a data.frame.
+  regression_tune_values_grid <- regression_tune_values
+  if (is.function(regression_tune_values)) {
+    regression_tune_values_grid <- regression_tune_values(x_train)
+    if (!is.data.frame(regression_tune_values_grid)) {
+      stop("The output of the user provided `regression_tune_values` function must be of class `data.frame`.")
+    }
+  }
+
+  # Get the names of the hyperparameters the user provided values for
+  regression_tune_values_names <- names(regression_tune_values_grid)
+
+  # Check that user have provided values for the hyperparameters to tune
   if (!(all(regression_tune_values_names %in% regression_para_tune_names) &&
     all(regression_para_tune_names %in% regression_tune_values_names))) {
     stop(paste0(
@@ -249,7 +277,7 @@ get_regression_tune <- function(regression_model, regression_tune_values) {
     ))
   }
 
-  # Return if we are to tune some model hyper-parameters
+  # Return if we are to tune some model hyperparameters
   return(regression_tune)
 }
 
@@ -265,17 +293,23 @@ get_regression_tune <- function(regression_model, regression_tune_values) {
 #' @keywords internal
 check_regression_parameters <- function(internal) {
   # Check that it is a function that returns the RHS of the formula for arbitrary feature name inputs
-  check_regression_recipe_func(internal$parameters$regression_recipe_func)
+  check_regression_recipe_func(regression_recipe_func = internal$parameters$regression_recipe_func)
 
   # Check that `regression_vfold_cv_para` is either NULL or a named list that only contains recognized parameters
-  check_regression_vfold_cv_para(internal$parameters$regression_vfold_cv_para)
+  check_regression_vfold_cv_para(regression_vfold_cv_para = internal$parameters$regression_vfold_cv_para)
 
   # Check that `check_regression_n_comb` is a valid value (only applicable for surrogate regression)
-  check_regression_n_comb(internal$parameters$regression_surr_n_comb, internal$parameters$used_n_combinations)
+  check_regression_n_comb(
+    regression_surr_n_comb = internal$parameters$regression_surr_n_comb,
+    used_n_combinations = internal$parameters$used_n_combinations
+  )
 
   # Check and get if we are to tune the hyperparameters of the regression model
-  internal$parameters$regression_tune <-
-    get_regression_tune(internal$parameters$regression_model, internal$parameters$regression_tune_values)
+  internal$parameters$regression_tune <- get_regression_tune(
+    regression_model = internal$parameters$regression_model,
+    regression_tune_values = internal$parameters$regression_tune_values,
+    x_train = internal$data$x_train
+  )
 
   return(internal)
 }
@@ -318,6 +352,11 @@ check_regression_vfold_cv_para <- function(regression_vfold_cv_para) {
         "The following parameters in `regression_vfold_cv_para` are not supported by `rsample::vfold_cv()`: '",
         paste0(unknown_para_names, collapse = "', '"), "'."
       ))
+    }
+
+    # Ensure that we have at least two folds in the cross validation procedure
+    if ("v" %in% names(regression_vfold_cv_para) && regression_vfold_cv_para[["v"]] <= 1) {
+      stop("The parameter `v` in `regression_vfold_cv_para` must be strictly larger than 1.")
     }
   }
 }
@@ -363,26 +402,27 @@ regression_prep_message_comb <- function(internal, index_features, comb_idx) {
 #' Produce message about which batch prepare_data is working on
 #'
 #' @param regression_results The results of the CV procedures.
-#' @param regression_tune_values Object containing the hyperparameter values.
+#' @param regression_grid Object containing the hyperparameter values.
 #' @param n_cv Integer (default is 10) specifying the number of CV hyperparameter configurations to print.
 #'
 #' @author Lars Henry Berge Olsen
 #' @keywords internal
-regression_cv_message <- function(regression_results, regression_tune_values, n_cv = 10) {
+regression_cv_message <- function(regression_results, regression_grid, n_cv = 10) {
   # Get the feature names and add evaluation metric rmse
-  feature_names <- names(regression_tune_values)
+  feature_names <- names(regression_grid)
   feature_names_rmse <- c(feature_names, "rmse", "rmse_std_err")
 
   # Let n_cv be the minimum of the provided value and the number of possible printouts
-  n_cv <- min(n_cv, nrow(regression_tune_values))
+  n_cv <- min(n_cv, nrow(regression_grid))
 
   # Extract the n_cv best results
   best_results <- tune::show_best(regression_results, n = n_cv)
 
   # Needed to make prinout tables prettier to ensure same column dimensions for all settings.
-  regression_tune_values$rmse <- round(best_results$mean, 2)
-  regression_tune_values$rmse_std <- round(best_results$std_err, 2)
-  width <- sapply(regression_tune_values, function(x) max(nchar(as.character(unique(x)))))
+  regression_grid_best = best_results[,feature_names]
+  regression_grid_best$rmse <- round(best_results$mean, 2)
+  regression_grid_best$rmse_std <- round(best_results$std_err, 2)
+  width <- sapply(regression_grid_best, function(x) max(nchar(as.character(unique(x)))))
 
   # Message title of the results
   message(paste0("Results of the ", best_results$n[1], "-fold cross validation (top ", n_cv, " best configurations):"))
