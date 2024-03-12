a50f65965d1b32a9ea6e2f2e8caeee0cef2e4372
LHBO <lholsen@math.uio.no>
2024-03-12 17:51:56 +0000

    Added regression to rebuild long running vignette

--- rebuild_long_running_vignette.R
+++ rebuild_long_running_vignette.R
@@ -5,13 +5,14 @@
 old_wd <- getwd()
 
 setwd("vignettes/")
-knitr::knit("understanding_shapr_vaeac.Rmd.orig", output = "understanding_shapr_vaeac.Rmd")
-#knitr::purl("understanding_shapr_vaeac.Rmd.orig", output = "understanding_shapr_vaeac.R") # Don't need this
 
 knitr::knit("understanding_shapr.Rmd.orig", output = "understanding_shapr.Rmd")
 #knitr::purl("understanding_shapr.Rmd.orig", output = "understanding_shapr.R") # Don't need this
 
-#knitr::knit("understanding_shapr_regression.Rmd.orig", output = "understanding_shapr_regression.Rmd")
+knitr::knit("understanding_shapr_vaeac.Rmd.orig", output = "understanding_shapr_vaeac.Rmd")
+#knitr::purl("understanding_shapr_vaeac.Rmd.orig", output = "understanding_shapr_vaeac.R") # Don't need this
+
+knitr::knit("understanding_shapr_regression.Rmd.orig", output = "understanding_shapr_regression.Rmd")
 #knitr::purl("understanding_shapr_regression.Rmd.orig", output = "understanding_shapr_regression.R") # Don't need this
 
 setwd(old_wd)
