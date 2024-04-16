3088ba6e66c2be68c024d30c81d49599fd5595a6
LHBO <lholsen@math.uio.no>
2024-04-16 13:02:28 +0200

    Changed comment in py-explain

--- python/shaprpy/explain.py
+++ python/shaprpy/explain.py
@@ -171,10 +171,12 @@ def explain(
     rinternal = shapr.setup_computation(rinternal, NULL, NULL)
 
     # Compute the v(S):
-    # MC: Get the samples for the conditional distributions with the specified approach
-    # Predict with these samples
-    # Perform MC integration on these to estimate the conditional expectation (v(S))
-    # Regression: directly estimate the conditional expectation (v(S)) using regression models
+    # MC:
+    # 1. Get the samples for the conditional distributions with the specified approach
+    # 2. Predict with these samples
+    # 3. Perform MC integration on these to estimate the conditional expectation (v(S))
+    # Regression:
+    # 1. Directly estimate the conditional expectation (v(S)) using the fitted regression model(s)
     rvS_list = compute_vS(rinternal, model, predict_model)
 
     timing_list["compute_vS"] = datetime.now()
