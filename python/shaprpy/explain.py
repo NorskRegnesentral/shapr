fe9c4a5601b86bab10ad4b79ecf27aa91313ab99
LHBO <lholsen@math.uio.no>
2024-04-16 21:24:40 +0200

    Made shaprpy-explain easier for Martin to look at. `timedelta` is not used anywhere.

--- python/shaprpy/explain.py
+++ python/shaprpy/explain.py
@@ -2,7 +2,7 @@ import warnings
 import numpy as np
 import pandas as pd
 from typing import Callable
-from datetime import datetime, timedelta
+from datetime import datetime
 import rpy2.robjects as ro
 from rpy2.robjects.packages import importr
 from rpy2.rinterface import NULL, NA
@@ -37,7 +37,7 @@ def explain(
     timing: bool = True,
     verbose: int | None = 0,
     **kwargs,
-):
+  ):
     '''Explain the output of machine learning models with more accurately estimated Shapley values.
 
     Computes dependence-aware Shapley values for observations in `x_explain` from the specified
@@ -137,22 +137,22 @@ def explain(
     # Checks the input parameters and their compatability
     # Checks data/model compatability
     rinternal = shapr.setup(
-      x_train = py2r(x_train),
-      x_explain = py2r(x_explain),
-      approach = approach,
-      prediction_zero = prediction_zero,
-      n_combinations = maybe_null(n_combinations),
-      group = r_group,
-      n_samples = n_samples,
-      n_batches = maybe_null(n_batches),
-      seed = seed,
-      keep_samp_for_vS = keep_samp_for_vS,
-      feature_specs = rfeature_specs,
-      MSEv_uniform_comb_weights = MSEv_uniform_comb_weights,
-      timing = timing,
-      verbose = verbose,
-      is_python = True,
-      **kwargs
+        x_train = py2r(x_train),
+        x_explain = py2r(x_explain),
+        approach = approach,
+        prediction_zero = prediction_zero,
+        n_combinations = maybe_null(n_combinations),
+        group = r_group,
+        n_samples = n_samples,
+        n_batches = maybe_null(n_batches),
+        seed = seed,
+        keep_samp_for_vS = keep_samp_for_vS,
+        feature_specs = rfeature_specs,
+        MSEv_uniform_comb_weights = MSEv_uniform_comb_weights,
+        timing = timing,
+        verbose = verbose,
+        is_python = True,
+        **kwargs
     )
 
     timing_list["setup"] = datetime.now()
@@ -207,12 +207,7 @@ def compute_vS(rinternal, model, predict_model):
   S_batch = rinternal.rx2('objects').rx2('S_batch')
   ret = ro.ListVector({})
   for i, S in enumerate(S_batch):
-    ret.rx2[i+1] = batch_compute_vS(
-      S=S,
-      rinternal=rinternal,
-      model=model,
-      predict_model=predict_model,
-    )
+    ret.rx2[i+1] = batch_compute_vS(S=S, rinternal=rinternal, model=model, predict_model=predict_model)
   return ret
 
 
@@ -237,14 +232,15 @@ def batch_prepare_vS_MC(S, rinternal, model, predict_model):
   dt_vS = shapr.compute_MCint(dt)
 
   if keep_samp_for_vS:
-      return ro.ListVector({'dt_vS': dt_vS, 'dt_samp_for_vS': dt})
+    return ro.ListVector({'dt_vS': dt_vS, 'dt_samp_for_vS': dt})
   else:
-      return dt_vS
+    return dt_vS
 
 
 def compute_preds(dt, feature_names, predict_model, model):
-  preds = predict_model(model, r2py(dt).loc[:, feature_names])
+  preds = predict_model(model, r2py(dt).loc[:,feature_names])
   return ro.r.cbind(dt, p_hat=ro.FloatVector(preds.tolist()))
+
 
 
 def get_feature_specs(get_model_specs, model):
@@ -263,30 +259,25 @@ def get_feature_specs(get_model_specs, model):
     try:
       feature_specs = get_model_specs(model)
     except Exception as e:
-      raise RuntimeError(
-        f'The get_model_specs function of class `{model_class0}` is invalid.\nA basic function test threw the following error:\n{e}')
+      raise RuntimeError(f'The get_model_specs function of class `{model_class0}` is invalid.\nA basic function test threw the following error:\n{e}')
 
   if not isinstance(feature_specs, dict):
-    raise ValueError(
-      f'`get_model_specs` returned an object of type `{type(feature_specs)}`, but it should be of type `dict`')
+    raise ValueError(f'`get_model_specs` returned an object of type `{type(feature_specs)}`, but it should be of type `dict`')
   if set(feature_specs.keys()) != set(["labels", "classes", "factor_levels"]):
-    raise ValueError(
-      f'`get_model_specs` should return a `dict` with keys ["labels","classes","factor_levels"], but found keys {list(feature_specs.keys())}')
+    raise ValueError(f'`get_model_specs` should return a `dict` with keys ["labels","classes","factor_levels"], but found keys {list(feature_specs.keys())}')
 
   if feature_specs is None:
     rfeature_specs = NULL
   else:
     py2r_or_na = lambda v: py2r(v) if v is not None else NA
-
     def strvec_or_na(v):
       if v is None: return NA
       strvec = ro.StrVector(list(v.values()))
       strvec.names = list(v.keys())
       return strvec
-
     def listvec_or_na(v):
       if v is None: return NA
-      return ro.ListVector({k: list(val) for k, val in v.items()})
+      return ro.ListVector({k: list(val) for k,val in v.items()})
 
     rfeature_specs = ro.ListVector({
       'labels': py2r_or_na(feature_specs['labels']),
@@ -297,29 +288,26 @@ def get_feature_specs(get_model_specs, model):
 
 
 def get_predict_model(x_test, predict_model, model):
+
   model_class0 = type(model)
 
   if (predict_model is not None) and (not callable(predict_model)):
-    raise RuntimeError(
-      f'The predict_model function of class `{model}` is invalid.\nA basic function test threw the following error:\n{e}')
+    raise RuntimeError(f'The predict_model function of class `{model}` is invalid.\nA basic function test threw the following error:\n{e}')
 
   if predict_model is None:
     predict_model = prebuilt_predict_model(model)
     if predict_model is None:
-      raise ValueError(
-        f'No pre-built predict_model for model of type {type(model)}. Please pass a custom predict_model to shaprpy.explain(...).')
+      raise ValueError(f'No pre-built predict_model for model of type {type(model)}. Please pass a custom predict_model to shaprpy.explain(...).')
 
   try:
     tmp = py2r(predict_model(model, x_test))
   except Exception as e:
-    raise RuntimeError(
-      f'The predict_model function of class `{model_class0}` is invalid.\nA basic function test threw the following error:\n{e}')
+    raise RuntimeError(f'The predict_model function of class `{model_class0}` is invalid.\nA basic function test threw the following error:\n{e}')
   if not all(base.is_numeric(tmp)):
     raise RuntimeError('The output of predict_model is expected to be numeric.')
   if not (len(tmp) == 2):
     raise RuntimeError('The output of predict_model does not match the length of the input.')
   return predict_model
-
 
 
 def prebuilt_get_model_specs(model):
@@ -352,10 +340,11 @@ def prebuilt_get_model_specs(model):
 
 
 def prebuilt_predict_model(model):
+
   # Look for sklearn
   try:
     from sklearn.base import is_classifier, is_regressor
-    if is_classifier(model): return lambda m, x: m.predict_proba(x)[:, 1]
+    if is_classifier(model): return lambda m, x: m.predict_proba(x)[:,1]
     if is_regressor(model): return lambda m, x: m.predict(x)
   except:
     pass
@@ -374,7 +363,7 @@ def prebuilt_predict_model(model):
     if isinstance(model, Model):
       def predict_fn(m, x):
         pred = m.predict(x)
-        return pred.reshape(pred.shape[0], )
+        return pred.reshape(pred.shape[0],)
 
       return predict_fn
   except:
@@ -385,13 +374,13 @@ def prebuilt_predict_model(model):
 
 def compute_time(timing_list):
   timing_secs = {
-    f'{key}': (timing_list[key] - timing_list[prev_key]).total_seconds()
-    for key, prev_key in zip(list(timing_list.keys())[1:], list(timing_list.keys())[:-1])
+      f'{key}': (timing_list[key] - timing_list[prev_key]).total_seconds()
+      for key, prev_key in zip(list(timing_list.keys())[1:], list(timing_list.keys())[:-1])
   }
   timing_output = {
-    'init_time': timing_list['init_time'].strftime("%Y-%m-%d %H:%M:%S"),
-    'total_time_secs': sum(timing_secs.values()),
-    'timing_secs': timing_secs
+      'init_time': timing_list['init_time'].strftime("%Y-%m-%d %H:%M:%S"),
+      'total_time_secs': sum(timing_secs.values()),
+      'timing_secs': timing_secs
   }
 
   return timing_output
