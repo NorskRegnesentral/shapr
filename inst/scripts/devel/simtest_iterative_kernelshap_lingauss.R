4dae11cc8790f124c5a773e04107c3b15c6992ff
Frida Svendal Aase <fsaase@nr.no>
2024-06-10 15:31:46 +0200

    Running simulations, saving resutls to file

--- inst/scripts/devel/simtest_iterative_kernelshap_lingauss.R
+++ inst/scripts/devel/simtest_iterative_kernelshap_lingauss.R
@@ -28,7 +28,6 @@ mu <- rep(0,m)
 set.seed(123)
 
 
-
 x_train <- as.data.table(MASS::mvrnorm(n_train,mu,Sigma))
 x_explain <- as.data.table(MASS::mvrnorm(n_explain,mu,Sigma))
 
@@ -49,15 +48,17 @@ p0 <- mean(y_train)
 
 ### First run proper shapr call on this
 
+sim_results_saving_folder = "../effektiv_shapley_output/"
+shapley_reweighting_strategy = "none"
 
-
-
+set.seed(465132)
+inds = 1:n_explain
 expl <- shapr::explain(model = model,
-                       x_explain= x_explain,
+                       x_explain= x_explain[inds,],
                        x_train = x_train,
                        approach = "gaussian",
                        prediction_zero = p0,Sigma=Sigma,mu=mu)
-
+fwrite(expl$shapley_values,paste0(sim_results_saving_folder,"exact_shapley_values", shapley_reweighting_strategy, ".csv"))
 
 
 cutoff_feats <- paste0("VV",1:12)
@@ -66,18 +67,16 @@ cutoff_feats <- paste0("VV",1:12)
 ### Need to create an lm analogoue to pred_mod_xgb here
 
 
-set.seed(123)
-
-ctree.mincriterion = 0.95
-ctree.minsplit = 20
-ctree.minbucket = 7
-ctree.sample = TRUE
+# ctree.mincriterion = 0.95
+# ctree.minsplit = 20
+# ctree.minbucket = 7
+# ctree.sample = TRUE
 
 
-n_samples <- 1000
-approach = "gaussian"
-gaussian.mu <- mu
-gaussian.cov_mat <- Sigma
+# n_samples <- 1000
+# approach = "gaussian"
+# gaussian.mu <- mu
+# gaussian.cov_mat <- Sigma
 
 # Reduce if < 10% prob of shapval > 0.2
 shapley_threshold_val <-  0.2
@@ -85,8 +84,9 @@ shapley_threshold_prob <- 0.1
 
 source("inst/scripts/devel/iterative_kernelshap_sourcefuncs.R")
 
-testObs_computed_vec <- seq_len(n_explain)
+testObs_computed_vec <- inds# seq_len(n_explain)
 runres_list <- runcomps_list <- list()
+
 
 for(kk in testObs_computed_vec){
   testObs_computed <- testObs_computed_vec[kk]
@@ -104,24 +104,47 @@ for(kk in testObs_computed_vec){
                               predict_model = predict.lm,
                               shapley_threshold_val = shapley_threshold_val,
                               shapley_threshold_prob = shapley_threshold_prob,
-                              approach = "gaussian")
+                              approach = "gaussian",
+                              n_samples = 1000,
+                              gaussian.mu = mu,
+                              gaussian.cov_mat = Sigma,
+                              shapley_reweighting_strategy = shapley_reweighting_strategy)
   runres_list[[kk]] <- run$kshap_final
   runcomps_list[[kk]] <- sum(sapply(run$keep_list,"[[","no_computed_combinations"))
   print(kk)
 }
 
-
 est <- rbindlist(runres_list)
 est[,other_features:=NULL]
+fwrite(est,paste0(sim_results_saving_folder,"iterative_shapley_values", shapley_reweighting_strategy, ".csv"))
+
 truth <- expl$shapley_values
 
-
-
+expl_approx <- matrix(0, nrow = length(inds), ncol = m+1)
+expl_approx_obj_list <- list()
+for (i in testObs_computed_vec){
+  expl_approx_obj <- shapr::explain(model = model,
+                        x_explain= x_explain[inds[i],],
+                        x_train = x_train,
+                        approach = "gaussian",
+                        prediction_zero = p0,Sigma=Sigma,mu=mu,
+                        n_combinations = runcomps_list[[i]])
+  expl_approx[i,] = unlist(expl_approx_obj$shapley_values)
+  expl_approx_obj_list[[i]] <- expl_approx_obj
+}
+expl_approx <- as.data.table(expl_approx)
+colnames(expl_approx) <- colnames(truth)
+fwrite(expl_approx,paste0(sim_results_saving_folder,"approx_shapley_values", shapley_reweighting_strategy, ".csv"))
 
 bias_vec <- colMeans(est-truth)
 rmse_vec <- sqrt(colMeans((est-truth)^2))
 mae_vec <- colMeans(abs(est-truth))
 
+bias_vec_approx <- colMeans(expl_approx-truth)
+rmse_vec_approx <- sqrt(colMeans((expl_approx-truth)^2))
+mae_vec_approx <- colMeans(abs(expl_approx-truth))
+
+save.image(paste0(sim_results_saving_folder, "iterative_kernelshap_lingauss_p12", shapley_reweighting_strategy, ".RData"))
 
 hist(unlist(runcomps_list),breaks = 20)
 
@@ -133,31 +156,29 @@ sum(unlist(run$kshap_final))
 full_pred
 
 # TODO: Må finne ut av hvorfor det ikke gir korrekt sum her...
-for(i in 1:18){
-  print(sum(unlist(run$keep_list[[i]]$kshap_est_dt[,-1])))+run$keep_list[[i]]$shap_it_excluded_features)
-#print(run$keep_list[[i]]$shap_it_excluded_features)
-}
+# Hvis det er noen variabler som ble ekskludert, så må jeg legge til disse i summen for å få prediksjonen til modellen.
+# for(i in 1:18){
+#   print(sum(unlist(run$keep_list[[i]]$kshap_est_dt[,-1]))+run$keep_list[[i]]$shap_it_excluded_features)
+# #print(run$keep_list[[i]]$shap_it_excluded_features)
+# }
 
-run$kshap_it_est_dt
-
-
-
-run$kshap_final
-expl$shapley_values
+# run$kshap_it_est_dt
 
 
 
-
-kshap_final <- copy(run$kshap_est_dt_list[,-1])
-setnafill(kshap_final,"locf")
-kshap_final[.N,] # final estimate
-
-sum(unlist(kshap_final[.N,]))
-
-sum(unlist(expl$shapley_values[testObs_computed,]))
+# run$kshap_final
+# expl$shapley_values
 
 
 
+
+# kshap_final <- copy(run$kshap_est_dt_list[,-1])
+# setnafill(kshap_final,"locf")
+# kshap_final[.N,] # final estimate
+
+# sum(unlist(kshap_final[.N,]))
+
+# sum(unlist(expl$shapley_values[testObs_computed,]))
 
 
 
@@ -165,30 +186,33 @@ sum(unlist(expl$shapley_values[testObs_computed,]))
 
 
 
-cutoff_feats <- paste0("VV",1:6)
-testObs_computed <- 5
-
-full_pred <- predict(model,x_explain)[5]
-p0 <- mean(y_train)
-pred_not_to_decompose <- sum(expl$shapley_values[5,VV7:VV9])
 
 
-run_minor <- iterative_kshap_func(model,x_explain,x_train,
-                            testObs_computed = 5,
-                            cutoff_feats = cutoff_feats,
-                            full_pred = full_pred,
-                            pred_not_to_decompose = pred_not_to_decompose,
-                            p0 = p0,
-                            predict_model = predict.lm,shapley_threshold_val = 0)
+
+# cutoff_feats <- paste0("VV",1:6)
+# testObs_computed <- 5
+
+# full_pred <- predict(model,x_explain)[5]
+# p0 <- mean(y_train)
+# pred_not_to_decompose <- sum(expl$shapley_values[5,VV7:VV9])
 
 
-aa=run$keep_list[[8]]$dt_vS
+# run_minor <- iterative_kshap_func(model,x_explain,x_train,
+#                             testObs_computed = 5,
+#                             cutoff_feats = cutoff_feats,
+#                             full_pred = full_pred,
+#                             pred_not_to_decompose = pred_not_to_decompose,
+#                             p0 = p0,
+#                             predict_model = predict.lm,shapley_threshold_val = 0)
 
-bb=run_minor$keep_list[[6]]$dt_vS
-setnames(bb,"p_hat_1","p_hat_1_approx")
 
-cc=merge(aa,bb)
-cc[,diff:=p_hat_1-p_hat_1_approx]
+# aa=run$keep_list[[8]]$dt_vS
+
+# bb=run_minor$keep_list[[6]]$dt_vS
+# setnames(bb,"p_hat_1","p_hat_1_approx")
+
+# cc=merge(aa,bb)
+# cc[,diff:=p_hat_1-p_hat_1_approx]
 
 
 # TODO:
