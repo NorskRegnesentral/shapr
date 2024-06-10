beec97f3dcc3e1a6fa170914f04d20077309f52f
Frida Svendal Aase <fsaase@nr.no>
2024-06-10 15:38:07 +0200

    Removed unecessary variables

--- inst/scripts/devel/simtest_iterative_kernelshap_lingauss.R
+++ inst/scripts/devel/simtest_iterative_kernelshap_lingauss.R
@@ -78,12 +78,6 @@ approach = "gaussian"
 gaussian.mu <- mu
 gaussian.cov_mat <- Sigma
 
-ctree.mincriterion = 0.95
-ctree.minsplit = 20
-ctree.minbucket = 7
-ctree.sample = TRUE
-
-
 # Reduce if < 10% prob of shapval > 0.2
 shapley_threshold_val <-  0.2
 shapley_threshold_prob <- 0.1
