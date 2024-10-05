0e0fbf0b6bc3848e7c2dd65f6cdf00498138de67
LHBO <lholsen@math.uio.no>
2024-10-05 17:37:48 +0200

    Updated vaeac

--- R/approach_vaeac.R
+++ R/approach_vaeac.R
@@ -41,11 +41,10 @@ setup_approach.vaeac <- function(internal, # add default values for vaeac here.
 
   # Extract the objects we will use later
   iter <- length(internal$iter_list)
-
   X <- internal$iter_list[[iter]]$X
   S <- internal$iter_list[[iter]]$S
+  S_causal <- internal$iter_list[[iter]]$S_causal_steps_unique_S # NULL if not causal sampling
   parameters <- internal$parameters
-
 
   # Check if we are doing a combination of approaches
   combined_approaches <- length(parameters$approach) > 1
@@ -64,10 +63,8 @@ setup_approach.vaeac <- function(internal, # add default values for vaeac here.
   vaeac_main_para <- mget(vaeac_main_para_names)
 
   # Add the default extra parameter values for the non-user specified extra parameters
-  parameters$vaeac.extra_parameters <- utils::modifyList(vaeac_get_extra_para_default(),
-    parameters$vaeac.extra_parameters,
-    keep.null = TRUE
-  )
+  parameters$vaeac.extra_parameters <-
+    utils::modifyList(vaeac_get_extra_para_default(), parameters$vaeac.extra_parameters, keep.null = TRUE)
 
   # Add the default main parameter values for the non-user specified main parameters
   parameters <- utils::modifyList(vaeac_main_para, parameters, keep.null = TRUE)
@@ -76,7 +73,18 @@ setup_approach.vaeac <- function(internal, # add default values for vaeac here.
   parameters <- c(parameters[(length(vaeac_main_para) + 1):length(parameters)], parameters[seq_along(vaeac_main_para)])
 
   # Check if vaeac is to be applied on a subset of coalitions.
-  if (!parameters$exact || parameters$is_groupwise || combined_approaches) {
+  if (!is.null(S_causal)) {
+    # We are doing causal Shapley values. Then we do not want to train on the full
+    # coalitions, but rather the coalitions in the chain of sampling steps used
+    # to generate the full MC sample. Casual Shapley does not support combined
+    # approaches, so we do not have to check for that. All coalitions are
+    # done by vaeac, and we give them equal importance. Skip the empty and grand coalitions.
+    # Note that some steps occur more often (when features in Sbar are late in the causal ordering),
+    # and one can potentially consider to give this more weight.
+    nrow_S_causal <- nrow(S_causal)
+    parameters$vaeac.extra_parameters$vaeac.mask_gen_coalitions <- S_causal[-c(1, nrow_S_causal), , drop = FALSE]
+    parameters$vaeac.extra_parameters$vaeac.mask_gen_coalitions_prob <- rep(1, nrow_S_causal - 2) / (nrow_S_causal - 2)
+  } else if (!parameters$exact || parameters$is_groupwise || combined_approaches) {
     # We have either:
     # 1) sampled `n_coalitions` different subsets of coalitions (i.e., not exact),
     # 2) using the coalitions which respects the groups in group Shapley values, and/or
@@ -1838,9 +1846,13 @@ vaeac_get_mask_generator_name <- function(mask_gen_coalitions,
     mask_generator_name <- "mcar_mask_generator"
 
     # Small printout
-    if ("vS_details" %in% verbose) cli::cli_text(paste0("Using 'mcar_mask_generator' with 'masking_ratio = ",
-                                                        masking_ratio,
-                                                        "'."))
+    if ("vS_details" %in% verbose) {
+      cli::cli_text(paste0(
+        "Using 'mcar_mask_generator' with 'masking_ratio = ",
+        masking_ratio,
+        "'."
+      ))
+    }
   } else if (length(masking_ratio) > 1) {
     # We are going to use 'specified_prob_mask_generator' as masking_ratio is a vector (of same length as ncol(x_train).
     # I.e., masking_ratio[5] specifies the probability of masking 5 features
