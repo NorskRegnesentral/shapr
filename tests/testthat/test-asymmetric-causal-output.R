496f8008223cd660decff5caf4232d8f0d3d06e7
LHBO <lholsen@math.uio.no>
2024-10-06 14:56:10 +0200

    updated test output file

--- tests/testthat/test-asymmetric-causal-output.R
+++ tests/testthat/test-asymmetric-causal-output.R
@@ -1,14 +1,13 @@
 # Continuous data -------------------------------------------------------------------------------------------------
 test_that("output_asymmetric_conditional", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = NULL,
@@ -20,15 +19,14 @@ test_that("output_asymmetric_conditional", {
 
 test_that("output_asymmetric_conditional_regression", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "regression_separate",
       regression.model = parsnip::linear_reg(),
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = NULL
@@ -39,14 +37,13 @@ test_that("output_asymmetric_conditional_regression", {
 
 test_that("output_symmetric_conditional", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(1:2, 3, 4:5), # Does not matter when asymmetric = TRUE and confounding = NULL
       confounding = NULL,
@@ -58,14 +55,13 @@ test_that("output_symmetric_conditional", {
 
 test_that("output_symmetric_marginal_independence", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "independence",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(1:5),
       confounding = TRUE,
@@ -77,14 +73,13 @@ test_that("output_symmetric_marginal_independence", {
 
 test_that("output_symmetric_marginal_gaussian", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(1:5),
       confounding = TRUE,
@@ -96,14 +91,13 @@ test_that("output_symmetric_marginal_gaussian", {
 
 test_that("output_asymmetric_causal_confounding_TRUE", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = TRUE,
@@ -113,16 +107,17 @@ test_that("output_asymmetric_causal_confounding_TRUE", {
   )
 })
 
+
+
 test_that("output_asymmetric_causal_confounding_FALSE", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = FALSE,
@@ -134,14 +129,13 @@ test_that("output_asymmetric_causal_confounding_FALSE", {
 
 test_that("output_asymmetric_causal_confounding_mix", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = c(TRUE, FALSE, FALSE),
@@ -151,36 +145,34 @@ test_that("output_asymmetric_causal_confounding_mix", {
   )
 })
 
-test_that("output_asymmetric_causal_confounding_mix_n_comb", {
+test_that("output_asymmetric_causal_confounding_mix_n_coal", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = c(TRUE, FALSE, FALSE),
       n_MC_samples = 5, # Just for speed
-      n_combinations = 6
+      max_n_coalitions = 6
     ),
-    "output_asymmetric_causal_confounding_mix_n_comb"
+    "output_asymmetric_causal_confounding_mix_n_coal"
   )
 })
 
 test_that("output_asymmetric_causal_confounding_mix_empirical", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "empirical",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = c(TRUE, FALSE, FALSE),
@@ -192,14 +184,13 @@ test_that("output_asymmetric_causal_confounding_mix_empirical", {
 
 test_that("output_asymmetric_causal_confounding_mix_ctree", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "empirical",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = c(TRUE, FALSE, FALSE),
@@ -211,14 +202,13 @@ test_that("output_asymmetric_causal_confounding_mix_ctree", {
 
 test_that("output_symmetric_causal_confounding_TRUE", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = TRUE,
@@ -230,14 +220,13 @@ test_that("output_symmetric_causal_confounding_TRUE", {
 
 test_that("output_symmetric_causal_confounding_FALSE", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = FALSE,
@@ -249,14 +238,13 @@ test_that("output_symmetric_causal_confounding_FALSE", {
 
 test_that("output_symmetric_causal_confounding_mix", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = c(TRUE, FALSE, FALSE),
@@ -270,14 +258,13 @@ test_that("output_symmetric_causal_confounding_mix", {
 ## Group-wise  -----------------------------------------------------------------------------------------------------
 test_that("output_symmetric_causal_confounding_TRUE_group", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(1:2, 3),
       confounding = TRUE,
@@ -291,14 +278,13 @@ test_that("output_symmetric_causal_confounding_TRUE_group", {
 
 test_that("output_symmetric_causal_confounding_mix_group", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_numeric,
       x_explain = x_explain_numeric,
       x_train = x_train_numeric,
       approach = "gaussian",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(1, 2, 3),
       confounding = c(TRUE, TRUE, FALSE),
@@ -315,14 +301,13 @@ test_that("output_symmetric_causal_confounding_mix_group", {
 # Mixed data ------------------------------------------------------------------------------------------------------
 test_that("output_mixed_symmetric_causal_confounding_TRUE", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_mixed,
       x_explain = x_explain_mixed,
       x_train = x_train_mixed,
       approach = "ctree",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = TRUE,
@@ -334,14 +319,13 @@ test_that("output_mixed_symmetric_causal_confounding_TRUE", {
 
 test_that("output_mixed_asymmetric_causal_confounding_mixed", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_mixed,
       x_explain = x_explain_mixed,
       x_train = x_train_mixed,
       approach = "ctree",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = c(TRUE, FALSE, FALSE),
@@ -353,14 +337,13 @@ test_that("output_mixed_asymmetric_causal_confounding_mixed", {
 
 test_that("output_mixed_asymmetric_causal_confounding_mixed_2", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_mixed,
       x_explain = x_explain_mixed,
       x_train = x_train_mixed,
       approach = "ctree",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = c(FALSE, TRUE, TRUE),
@@ -373,15 +356,14 @@ test_that("output_mixed_asymmetric_causal_confounding_mixed_2", {
 
 test_that("output_mixed_asymmetric_conditional_regression", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_mixed,
       x_explain = x_explain_mixed,
       x_train = x_train_mixed,
       approach = "regression_separate",
       regression.model = parsnip::linear_reg(),
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = TRUE,
       causal_ordering = list(1:2, 3, 4:5),
       confounding = NULL
@@ -395,14 +377,13 @@ test_that("output_mixed_asymmetric_conditional_regression", {
 # Categorical data ------------------------------------------------------------------------------------------------
 test_that("output_categorical_asymmetric_causal_mixed_categorical", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_categorical,
       x_explain = x_explain_categorical,
       x_train = x_train_categorical,
       approach = "categorical",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(3:4, 2, 1),
       confounding = c(TRUE, FALSE, FALSE),
@@ -414,14 +395,13 @@ test_that("output_categorical_asymmetric_causal_mixed_categorical", {
 
 test_that("output_categorical_asymmetric_causal_mixed_ctree", {
   expect_snapshot_rds(
-    shapr::explain(
+    explain(
+      testing = TRUE,
       model = model_lm_categorical,
       x_explain = x_explain_categorical,
       x_train = x_train_categorical,
       approach = "ctree",
       prediction_zero = p0,
-      n_batches = 1,
-      timing = FALSE,
       asymmetric = FALSE,
       causal_ordering = list(3:4, 2, 1),
       confounding = c(TRUE, FALSE, FALSE),
