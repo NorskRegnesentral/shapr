e67c5add7f150784d0c7e8fd3eb79a8c13c979ea
LHBO <lholsen@math.uio.no>
2024-10-07 23:45:56 +0200

    rcpp update

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -148,7 +148,7 @@ prepare_data_copula_cpp <- function(MC_samples_mat, x_explain_mat, x_explain_gau
 #' observations to explain after being transformed using the Gaussian transform, i.e., the samples have been
 #' transformed to a standardized normal distribution.
 #' @param x_train_mat arma::mat. Matrix of dimension (`n_train`, `n_features`) containing the training observations.
-#' @param S arma::mat. Matrix of dimension (`n_combinations`, `n_features`) containing binary representations of
+#' @param S arma::mat. Matrix of dimension (`n_coalitions`, `n_features`) containing binary representations of
 #' the used coalitions. S cannot contain the empty or grand coalition, i.e., a row containing only zeros or ones.
 #' This is not a problem internally in shapr as the empty and grand coalitions treated differently.
 #' @param mu arma::vec. Vector of length `n_features` containing the mean of each feature after being transformed
@@ -156,10 +156,6 @@ prepare_data_copula_cpp <- function(MC_samples_mat, x_explain_mat, x_explain_gau
 #' @param cov_mat arma::mat. Matrix of dimension (`n_features`, `n_features`) containing the pairwise covariance
 #' between all pairs of features after being transformed using the Gaussian transform, i.e., the samples have been
 #' transformed to a standardized normal distribution.
-#'
-#' @return An arma::mat/2D array of dimension (`n_MC_samples`, `n_explain` * `n_coalitions`, `n_features`), where
-#' the columns (_,j,_) are matrices of dimension (`n_MC_samples`, `n_features`) containing the conditional Gaussian
-#' copula MC samples for each explicand and coalition on the original scale.
 #'
 #' @return An arma::mat/2D array of dimension (`n_explain` * `n_coalitions`, `n_features`),
 #' where the rows (n_explain * S_ind, n_explain * (S_ind + 1) - 1) contains the single
