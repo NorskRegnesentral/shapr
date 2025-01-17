a930933ab04cd28f1bf0cecb44ef1ea14d1a101d
Martin <jullum@nr.no>
2025-01-17 09:58:37 +0100

    [py] edit r package installation

--- python/install_r_packages.R
+++ python/install_r_packages.R
@@ -1,4 +1,2 @@
 # Installs the required R-packages
-install.packages("remotes", repos = "https://cloud.r-project.org")
-remotes::install_github("NorskRegnesentral/shapr")
-# Installs the development version of shapr from the master branch on CRAN
+install.packages("shapr")
