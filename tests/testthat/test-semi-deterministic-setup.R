8f1b4211350b0093e7e038c4fe50011d9b7cc798
Lars Henry Berge Olsen <lhbolsen@nr.no>
2025-04-08 15:41:48 +0200

    added setup tests for semi-deterministic sampling

--- tests/testthat/test-semi-deterministic-setup.R
+++ tests/testthat/test-semi-deterministic-setup.R
@@ -1,7 +1,6 @@
-test_that("semi_deterministic_samplign when not paired sampling", {
+test_that("semi_deterministic_samplign: not paired sampling", {
   expect_snapshot(
     {
-      # no regression model passed
       explain(
         testing = TRUE,
         model = model_lm_numeric,
@@ -12,6 +11,55 @@ test_that("semi_deterministic_samplign when not paired sampling", {
         seed = 1,
         extra_computation_args = list(
           paired_shap_sampling = FALSE,
+          semi_deterministic_sampling = TRUE
+        )
+      )
+    },
+    error = TRUE
+  )
+})
+
+test_that("semi_deterministic_samplign: not regular sampling", {
+  expect_snapshot(
+    {
+      explain_forecast(
+        testing = TRUE,
+        model = model_ar_temp,
+        y = data_arima[, "Temp"],
+        train_idx = 2:151,
+        explain_idx = 152:153,
+        explain_y_lags = 2,
+        horizon = 3,
+        approach = "empirical",
+        phi0 = p0_ar,
+        seed = 1,
+        group_lags = FALSE,
+        extra_computation_args = list(
+          paired_shap_sampling = TRUE,
+          semi_deterministic_sampling = TRUE
+        )
+      )
+    },
+    error = TRUE
+  )
+})
+
+test_that("semi_deterministic_samplign: not symmetric sampling", {
+  expect_snapshot(
+    {
+      explain(
+        testing = TRUE,
+        model = model_lm_numeric,
+        x_explain = x_explain_numeric,
+        x_train = x_train_numeric,
+        approach = "gaussian",
+        phi0 = p0,
+        seed = 1,
+        asymmetric = TRUE,
+        causal_ordering = list(1:2, 3, 4:5),
+        confounding = NULL,
+        extra_computation_args = list(
+          paired_shap_sampling = TRUE,
           semi_deterministic_sampling = TRUE
         )
       )
