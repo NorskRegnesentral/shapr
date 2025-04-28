e9e395e3cb44cd31de0098acefa8f7cd00a9a22d
Martin <jullum@nr.no>
2025-04-28 09:39:03 +0200

    .pkg for all model_ files

--- R/model_xgboost.R
+++ R/model_xgboost.R
@@ -2,7 +2,7 @@
 #' @export
 predict_model.xgb.Booster <- function(x, newdata, ...) {
   if (!requireNamespace("xgboost", quietly = TRUE)) {
-    cli::cli_abort("The xgboost package is required for predicting xgboost models")
+    cli::cli_abort("The {.pkg xgboost} package is required for predicting `xgboost` models")
   }
 
   predict(x, as.matrix(newdata))
@@ -31,8 +31,8 @@ model_checker.xgb.Booster <- function(x) {
   ) {
     cli::cli_abort(
       paste0(
-        "We currently don't support multi-classification using xgboost, i.e. ",
-        "where num_class is greater than 2."
+        "We currently don't support multi-classification using `xgboost`, i.e. ",
+        "where `num_class` is greater than 2."
       )
     )
   }
@@ -41,8 +41,8 @@ model_checker.xgb.Booster <- function(x) {
     cli::cli_abort(
       paste0(
         "We currently don't support standard classification, which predicts the class directly. ",
-        "To train an xgboost model predicting the class probabilities, you'll need to change ",
-        "the objective to 'binary:logistic'"
+        "To train an `xgboost` model predicting the class probabilities, you'll need to change ",
+        "the objective to `binary:logistic`"
       )
     )
   }
