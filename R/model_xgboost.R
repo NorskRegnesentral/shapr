bbc77cdad6c72c3aef9d32d8cfc6309dd73f35b5
Martin Jullum <jullum@nr.no>
2025-12-07 23:01:09 +0100

    updated classes and testing script.

--- R/model_xgboost.R
+++ R/model_xgboost.R
@@ -1,127 +1,5 @@
 #' @rdname predict_model
 #' @export
-predict_model.xgb.Booster_old <- function(x, newdata, ...) {
-  if (!requireNamespace("xgboost", quietly = TRUE)) {
-    cli::cli_abort("The {.pkg xgboost} package is required for predicting `xgb.Booster` models.")
-  }
-
-  predict(x, as.matrix(newdata))
-}
-
-#' @rdname get_model_specs
-#' @export
-get_model_specs.xgb.Booster_old <- function(x) {
-  model_checker(x) # Checking if the model is supported
-
-  feature_specs <- list()
-  feature_specs$labels <- x$feature_names
-  m <- length(feature_specs$labels)
-
-  feature_specs$classes <- setNames(rep(NA, m), feature_specs$labels) # Not supported
-  feature_specs$factor_levels <- setNames(vector("list", m), feature_specs$labels)
-
-  return(feature_specs)
-}
-
-#' @rdname model_checker
-#' @export
-model_checker.xgb.Booster_old <- function(x) {
-  if (!is.null(x$params$objective) &&
-    (x$params$objective == "multi:softmax" || x$params$objective == "multi:softprob")
-  ) {
-    cli::cli_abort(
-      paste0(
-        "We currently do not support multiclass classification using `xgboost`, i.e., ",
-        "when `num_class` is greater than 2."
-      )
-    )
-  }
-
-  if (!is.null(x$params$objective) && x$params$objective == "reg:logistic") {
-    cli::cli_abort(
-      paste0(
-        "We currently do not support standard classification, which predicts the class directly. ",
-        "To train an `xgboost` model that predicts class probabilities, change ",
-        "the objective to `binary:logistic`."
-      )
-    )
-  }
-  return(NULL)
-}
-
-### Corresponding functions for the xgboost >= 3.1.2.1 models
-
-#' @rdname predict_model
-#' @export
-predict_model.xgb.Booster <- function(x, newdata, ...) {
-  if (!requireNamespace("xgboost", quietly = TRUE)) {
-    cli::cli_abort("The {.pkg xgboost} package is required for predicting `xgboost` models.")
-  }
-
-  predict(x, newdata)
-}
-
-#' @rdname get_model_specs
-#' @export
-get_model_specs.xgb.Booster <- function(x) {
-  model_checker(x) # Checking if the model is supported
-
-  feature_specs <- list()
-  feature_specs$labels <- xgboost::getinfo(x,"feature_name")
-  m <- length(feature_specs$labels)
-
-  feature_specs$classes <- xgboost::getinfo(x,"feature_type")
-
-  if(is.null(feature_specs$classes)){
-    # Sometimes NULL is returned. Then we just set NA's (not supported)
-    # Typically the case when data are passed to the model as a numeric matrix
-    feature_specs$classes <- rep(NA, m)
-  }
-
-  # Mapping to generic classes
-  feature_specs$classes[feature_specs$classes %in% c("int", "float")] <- "numeric"
-  feature_specs$classes[feature_specs$classes %in% c("c")] <- "factor"
-
-  # Set names
-  names(feature_specs$classes) <- feature_specs$labels
-
-  # Factor levels is not supported
-  feature_specs$factor_levels <- setNames(as.list(rep(NA,m)), feature_specs$labels)
-
-  return(feature_specs)
-}
-
-#' @rdname model_checker
-#' @export
-model_checker.xgb.Booster <- function(x) {
-  objective <- xgboost::xgb.config(x)$learner$objective$name
-
-  if (!is.null(objective) &&
-      (objective == "multi:softmax" || objective == "multi:softprob")
-  ) {
-    cli::cli_abort(
-      paste0(
-        "We currently do not support multiclass classification using `xgboost`, i.e., ",
-        "when `num_class` is greater than 2."
-      )
-    )
-  }
-
-  if (!is.null(objective) && objective == "reg:logistic") {
-    cli::cli_abort(
-      paste0(
-        "We currently do not support standard classification, which predicts the class directly. ",
-        "To train an `xgboost` model that predicts class probabilities, change ",
-        "the objective to `binary:logistic`."
-      )
-    )
-  }
-  return(NULL)
-}
-
-
-#' @rdname predict_model
-#' @export
 predict_model.xgboost <- function(x, newdata, ...) {
   if (!requireNamespace("xgboost", quietly = TRUE)) {
     cli::cli_abort("The {.pkg xgboost} package is required for predicting `xgboost` models.")
@@ -136,12 +14,12 @@ get_model_specs.xgboost <- function(x) {
   model_checker(x) # Checking if the model is supported
 
   feature_specs <- list()
-  feature_specs$labels <- xgboost::getinfo(x,"feature_name")
+  feature_specs$labels <- xgboost::getinfo(x, "feature_name")
   m <- length(feature_specs$labels)
 
-  feature_specs$classes <- xgboost::getinfo(x,"feature_type")
+  feature_specs$classes <- xgboost::getinfo(x, "feature_type")
 
-  if(is.null(feature_specs$classes)){
+  if (is.null(feature_specs$classes)) {
     # Sometimes NULL is returned. Then we just set NA's (not supported)
     # Typically the case when data are passed to the model as a numeric matrix
     feature_specs$classes <- rep(NA, m)
@@ -155,7 +33,7 @@ get_model_specs.xgboost <- function(x) {
   names(feature_specs$classes) <- feature_specs$labels
 
   # Factor levels is not supported
-  feature_specs$factor_levels <- setNames(as.list(rep(NA,m)), feature_specs$labels)
+  feature_specs$factor_levels <- setNames(as.list(rep(NA, m)), feature_specs$labels)
 
   return(feature_specs)
 }
@@ -166,7 +44,7 @@ model_checker.xgboost <- function(x) {
   objective <- xgboost::xgb.config(x)$learner$objective$name
 
   if (!is.null(objective) &&
-      (objective == "multi:softmax" || objective == "multi:softprob")
+    (objective == "multi:softmax" || objective == "multi:softprob")
   ) {
     cli::cli_abort(
       paste0(
@@ -177,6 +55,71 @@ model_checker.xgboost <- function(x) {
   }
 
   if (!is.null(objective) && objective == "reg:logistic") {
+    cli::cli_abort(
+      paste0(
+        "We currently do not support standard classification, which predicts the class directly. ",
+        "To train an `xgboost` model that predicts class probabilities, change ",
+        "the objective to `binary:logistic`."
+      )
+    )
+  }
+  return(NULL)
+}
+
+### Corresponding functions for the xgboost < 3.1.2.1 models (xgb.Booster)
+
+#' @rdname predict_model
+#' @export
+predict_model.xgb.Booster <- function(x, newdata, ...) {
+  if (!requireNamespace("xgboost", quietly = TRUE)) {
+    cli::cli_abort("The {.pkg xgboost} package is required for predicting `xgb.Booster` models.")
+  }
+
+  if (utils::packageVersion("xgboost") >= "3.1.2.1") {
+    predict_model.xgboost(x, newdata, ...)
+  } else {
+    predict(x, as.matrix(newdata))
+  }
+}
+
+#' @rdname get_model_specs
+#' @export
+get_model_specs.xgb.Booster <- function(x) {
+  if (utils::packageVersion("xgboost") >= "3.1.2.1") {
+    return(get_model_specs.xgboost(x))
+  }
+
+  model_checker(x) # Checking if the model is supported
+
+  feature_specs <- list()
+  feature_specs$labels <- x$feature_names
+  m <- length(feature_specs$labels)
+
+  feature_specs$classes <- setNames(rep(NA, m), feature_specs$labels) # Not supported
+  feature_specs$factor_levels <- setNames(vector("list", m), feature_specs$labels)
+
+  return(feature_specs)
+}
+
+#' @rdname model_checker
+#' @export
+model_checker.xgb.Booster <- function(x) {
+  if (utils::packageVersion("xgboost") >= "3.1.2.1") {
+    return(model_checker.xgboost(x))
+  }
+
+  if (!is.null(x$params$objective) &&
+    (x$params$objective == "multi:softmax" || x$params$objective == "multi:softprob")
+  ) {
+    cli::cli_abort(
+      paste0(
+        "We currently do not support multiclass classification using `xgboost`, i.e., ",
+        "when `num_class` is greater than 2."
+      )
+    )
+  }
+
+  if (!is.null(x$params$objective) && x$params$objective == "reg:logistic") {
     cli::cli_abort(
       paste0(
         "We currently do not support standard classification, which predicts the class directly. ",
--- inst/devel/test_xgboost_new.R
+++ inst/devel/test_xgboost_new.R
@@ -1,8 +1,13 @@
 
+# Uncomment the following line to test with the OLD xgboost version (1.7.7.1)
+# .libPaths(c("/home/jullum/R/x86_64-pc-linux-gnu-library/4.3_old_xgboost", .libPaths()))
+
 #library(shapr)
 devtools::load_all()
 library(xgboost)
 library(data.table)
+
+message("Using xgboost version: ", packageVersion("xgboost"))
 
 # Set seed for reproducibility
 set.seed(123)
@@ -31,65 +36,113 @@ X_explain_num <- X_num[-train_idx, ][1:5, ] # Explain 5 observations
 # A. xgboost::xgboost with data.table
 # ------------------------------------------------------------------------------
 message("\n--- A. xgboost::xgboost with data.table ---")
-model_num_dt <- xgboost(
-  data = X_train_num,
-  label = y_train_num,
-  nrounds = 10,
-  verbose = 0
-)
+tryCatch({
+  set.seed(123)
+  model_num_dt <- xgboost(
+    data = X_train_num,
+    label = y_train_num,
+    nrounds = 10,
+    verbose = 0,
+    seed = 123
+  )
+  message("Prediction on head(X_train_num):")
+  print(head(predict(model_num_dt, X_train_num)))
 
-expl_num_dt <- explain(
-  model = model_num_dt,
-  x_train = X_train_num,
-  x_explain = X_explain_num,
-  x_train_explain = X_train_num,
-  approach = "ctree",
-  phi0  = mean(y_train_num)
-)
-expl_num_dt
+  set.seed(123)
+  expl_num_dt <- explain(
+    model = model_num_dt,
+    x_train = X_train_num,
+    x_explain = X_explain_num,
+    x_train_explain = X_train_num,
+    approach = "ctree",
+    phi0  = mean(y_train_num)
+  )
+  print(expl_num_dt)
+}, error = function(e) {
+  message("Skipping A (xgboost with data.table) due to error: ", e$message)
+})
 
 # ------------------------------------------------------------------------------
 # B. xgboost::xgboost with matrix
 # ------------------------------------------------------------------------------
 message("\n--- B. xgboost::xgboost with matrix ---")
-model_num_mat <- xgboost(
-  data = as.matrix(X_train_num),
-  label = y_train_num,
-  nrounds = 10,
-  verbose = 0
-)
+tryCatch({
+  set.seed(123)
+  model_num_mat <- xgboost(
+    data = as.matrix(X_train_num),
+    label = y_train_num,
+    nrounds = 10,
+    verbose = 0
+  )
+  message("Prediction on head(as.matrix(X_train_num)):")
+  print(head(predict(model_num_mat, as.matrix(X_train_num))))
 
-expl_num_mat <- explain(
-  model = model_num_mat,
-  x_train = X_train_num,
-  x_explain = X_explain_num,
-  approach = "empirical",
-  phi0 = mean(y_train_num)
-)
-expl_num_mat
-
+  set.seed(123)
+  expl_num_mat <- explain(
+    model = model_num_mat,
+    x_train = X_train_num,
+    x_explain = X_explain_num,
+    approach = "empirical",
+    phi0 = mean(y_train_num)
+  )
+  print(expl_num_mat)
+}, error = function(e) {
+  message("Skipping B (xgboost with matrix) due to error: ", e$message)
+})
 
 # ------------------------------------------------------------------------------
 # C. xgboost::xgb.train with xgb.DMatrix
 # ------------------------------------------------------------------------------
 message("\n--- C. xgboost::xgb.train with xgb.DMatrix ---")
-dtrain_num <- xgb.DMatrix(data = X_train_num, label = y_train_num)
-model_num_dmat <- xgb.train(
-  data = dtrain_num,
-  nrounds = 10,
-  verbose = 0
-)
+tryCatch({
+  dtrain_num <- xgb.DMatrix(data = as.matrix(X_train_num), label = y_train_num)
+  set.seed(123)
+  model_num_dmat <- xgb.train(
+    data = dtrain_num,
+    nrounds = 10,
+    verbose = 0
+  )
+  message("Prediction on head(dtrain_num):")
+  print(head(predict(model_num_dmat, dtrain_num)))
 
-# Note: For xgb.train models (xgb.Booster), shapr might expect matrix input
-# depending on implementation. Passing matrix here to be safe/standard.
-expl_num_dmat <- explain(
-  model = model_num_dmat,
-  x_train = X_train_num,
-  x_explain = X_explain_num,
-  approach = "empirical",
-  phi0 = mean(y_train_num)
-)
-expl_num_dmat
+  # Note: For xgb.train models (xgb.Booster), shapr might expect matrix input
+  # depending on implementation. Passing matrix here to be safe/standard.
+  set.seed(123)
+  expl_num_dmat <- explain(
+    model = model_num_dmat,
+    x_train = X_train_num,
+    x_explain = X_explain_num,
+    approach = "empirical",
+    phi0 = mean(y_train_num)
+  )
+  print(expl_num_dmat)
+}, error = function(e) {
+  message("Skipping C (xgb.train with xgb.DMatrix) due to error: ", e$message)
+})# ------------------------------------------------------------------------------
+# C2. xgboost::xgb.train with xgb.DMatrix (from data.table)
+# ------------------------------------------------------------------------------
+message("\n--- C2. xgboost::xgb.train with xgb.DMatrix (from data.table) ---")
+tryCatch({
+    dtrain_num_dt <- xgb.DMatrix(data = X_train_num, label = y_train_num)
+    model_num_dmat_dt <- xgb.train(
+        data = dtrain_num_dt,
+        nrounds = 10,
+        verbose = 0
+    )
+    message("Prediction on head(dtrain_num_dt):")
+    print(head(predict(model_num_dmat_dt, dtrain_num_dt)))
+
+    expl_num_dmat_dt <- explain(
+        model = model_num_dmat_dt,
+        x_train = X_train_num,
+        x_explain = X_explain_num,
+        approach = "empirical",
+        phi0 = mean(y_train_num)
+    )
+    print(expl_num_dmat_dt)
+}, error = function(e) {
+    message("Skipping C2 (xgb.train with xgb.DMatrix from data.table) due to error: ", e$message)
+})
 
 # ==============================================================================
 # 2. Categorical / Mixed Data
@@ -117,47 +170,99 @@ message("\n--- D. xgboost::xgboost with data.table (Factors) ---")
 # For newer xgboost, we might need `params = list(enable_categorical = TRUE)`?
 # Or it might handle it automatically if passed as DMatrix.
 # xgboost() function usually converts to DMatrix.
-model_cat_dt <- xgboost(
-data = X_train_cat,
-label = y_train_cat,
+tryCatch({
+  set.seed(123)
+  model_cat_dt <- xgboost(
+    data = X_train_cat,
+    label = y_train_cat,
     nrounds = 10,
-verbose = 0,
- params = list(enable_categorical = TRUE) # Uncomment if needed for specific version
-)
+    verbose = 0,
+    params = list(enable_categorical = TRUE) # Uncomment if needed for specific version
+  )
+  message("Prediction on head(X_train_cat):")
+  print(head(predict(model_cat_dt, X_train_cat)))
 
-expl_cat_dt <- explain(
+  set.seed(123)
+  expl_cat_dt <- explain(
     model = model_cat_dt,
     x_train = X_train_cat,
     x_explain = X_explain_cat,
     approach = "ctree",
-    phi0 = mean(y_train_cat),
-)
-expl_cat_dt
+    phi0 = mean(y_train_cat)
+  )
+  print(expl_cat_dt)
+}, error = function(e) {
+  message("Skipping D (xgboost with data.table factors) due to error: ", e$message)
+})
+
+# ------------------------------------------------------------------------------
+# E. xgboost::xgboost with matrix (Converted to Numeric)
+# ------------------------------------------------------------------------------
+message("\n--- E. xgboost::xgboost with matrix (Converted to Numeric) ---")
+tryCatch({
+  # Matrix input must be numeric. We convert factors to integer codes.
+  X_train_cat_num <- copy(X_train_cat)
+  X_train_cat_num[, cat1 := as.numeric(cat1)]
+  X_train_cat_num[, cat2 := as.numeric(cat2)]
+  X_explain_cat_num <- copy(X_explain_cat)
+  X_explain_cat_num[, cat1 := as.numeric(cat1)]
+  X_explain_cat_num[, cat2 := as.numeric(cat2)]
+
+  set.seed(123)
+  model_cat_mat <- xgboost(
+    data = as.matrix(X_train_cat_num),
+    label = y_train_cat,
+    nrounds = 10,
+    verbose = 0
+  )
+  message("Prediction on head(as.matrix(X_train_cat_num)):")
+  print(head(predict(model_cat_mat, as.matrix(X_train_cat_num))))
+
+  set.seed(123)
+  expl_cat_mat <- explain(
+    model = model_cat_mat,
+    x_train = as.matrix(X_train_cat_num),
+    x_explain = as.matrix(X_explain_cat_num),
+    x_train_explain = as.matrix(X_train_cat_num),
+    approach = "empirical",
+    phi0 = mean(y_train_cat)
+  )
+  print(expl_cat_mat)
+}, error = function(e) {
+  message("Skipping E (xgboost with matrix converted to numeric) due to error: ", e$message)
+})
 
 # ------------------------------------------------------------------------------
 # F. xgboost::xgb.train with xgb.DMatrix (Mixed)
 # ------------------------------------------------------------------------------
 message("\n--- F. xgboost::xgb.train with xgb.DMatrix (Mixed) ---")
 # Trying to create DMatrix from data.table with factors (New Feature Support)
+tryCatch({
+  # This might require enable_categorical=TRUE in xgb.DMatrix or params
+  dtrain_cat <- xgb.DMatrix(data = X_train_cat, label = y_train_cat, enable_categorical = TRUE)
 
-# This might require enable_categorical=TRUE in xgb.DMatrix or params
-dtrain_cat <- xgb.DMatrix(data = X_train_cat, label = y_train_cat)
+  set.seed(123)
+  model_cat_dmat <- xgb.train(
+    data = dtrain_cat,
+    nrounds = 10,
+    verbose = 0
+  )
+  message("Prediction on head(dtrain_cat):")
+  print(head(predict(model_cat_dmat, dtrain_cat)))
 
-model_cat_dmat <- xgb.train(
-data = dtrain_cat,
-nrounds = 10,
-verbose = 0
-)
-
-# For explain(), we pass the original data.table with factors.
-# shapr needs to handle this correctly with the model.
-expl_cat_dmat <- explain(
-model = model_cat_dmat,
-x_train = X_train_cat,
-x_explain = X_explain_cat,
-approach = "ctree",
-phi0 = mean(y_train_cat)
-)
-expl_cat_dmat
+  # For explain(), we pass the original data.table with factors.
+  # shapr needs to handle this correctly with the model.
+  set.seed(123)
+  expl_cat_dmat <- explain(
+    model = model_cat_dmat,
+    x_train = X_train_cat,
+    x_explain = X_explain_cat,
+    approach = "ctree",
+    phi0 = mean(y_train_cat)
+  )
+  print(expl_cat_dmat)
+}, error = function(e) {
+  message("Skipping F (xgb.train with DMatrix factors) due to error: ", e$message)
+})
 
 message("\nDone.")
